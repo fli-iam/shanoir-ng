f1454301569015b8c81d090a4a5cdf997c764aa6
yyao <yao.yao@inria.fr>
2016-12-16 17:03:40 +0100

    form validation and error management

--- shanoir-ng-front/src/main/webapp/app/users/edit/edit.user.component.ts
+++ shanoir-ng-front/src/main/webapp/app/users/edit/edit.user.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }  from '@angular/router';
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
-import {User} from '../shared/user.model';
-import {UserService} from '../shared/user.service';
-import {Role} from '../../roles/role.model';
-import {RoleService} from '../../roles/role.service';
+import { User } from '../shared/user.model';
+import { UserService } from '../shared/user.service';
+import { Role } from '../../roles/role.model';
+import { RoleService } from '../../roles/role.service';
 
 @Component({
     selector: 'editUser',
@@ -15,29 +16,19 @@ import {RoleService} from '../../roles/role.service';
 })
 
 export class EditUserComponent implements OnInit {
-    users: User[];
-    user: User;
+    user: User = new User();
     router: Router;
     userService: UserService;
     editUserForm: FormGroup;
     roles: Role[];
     roleService: RoleService;
+    isUserNameUnique: Boolean = true;
+    isEmailUnique: Boolean = true;
 
-    constructor(router: Router, userService: UserService, roleService: RoleService, fb:FormBuilder) {
+    constructor(router: Router, userService: UserService, roleService: RoleService, private fb: FormBuilder) {
         this.router = router;
         this.userService = userService;
         this.roleService = roleService;
-        this.editUserForm = fb.group({
-            'firstName': ['', Validators.required, Validators.minLength(2), Validators.maxLength(50)],
-            'lastName': ['', Validators.required, Validators.minLength(2), Validators.maxLength(50)],
-            'username': ['', Validators.required, Validators.minLength(4), Validators.maxLength(20)],
-            'email': ['', Validators.required],
-            'expirationDate': '',
-            'role': ['', Validators.required],
-            'canAccessToDicomAssociation': new FormControl('false'),
-            'isMedical': new FormControl('false'),
-            'motivation': ''
-        });
     }
 
     getRoles(): void {
@@ -46,34 +37,70 @@ export class EditUserComponent implements OnInit {
             .then(roles => this.roles = roles);
     }
 
-    cancel (): void {
+    cancel(): void {
         this.router.navigate(['../userlist']);
     }
-    
-    create(): void {
-        this.user = new User();
-        
-        this.user.firstName = this.editUserForm.value.firstName;
-        this.user.lastName = this.editUserForm.value.lastName;
-        this.user.username = this.editUserForm.value.username;
-        this.user.email = this.editUserForm.value.email;
-        this.user.expirationDate = this.editUserForm.value.expirationDate;
-        this.user.role = this.editUserForm.value.role;
-        this.user.canAccessToDicomAssociation = this.editUserForm.value.canAccessToDicomAssociation;
-        this.user.isMedical = this.editUserForm.value.isMedical;
-        this.user.motivation = this.editUserForm.value.motivation;
-        // to be removed
-        this.user.creationDate = new Date();
 
-        if (!this.user) {return;}
+    create(): void {
+        this.user = this.editUserForm.value;
+
         this.userService.create(this.user)
-            .then(user => {
+            .subscribe((user) => {
                 this.cancel();
+            }, (err: String) => {
+                if (err.indexOf("username unique") != -1) {
+                    this.isUserNameUnique = false;
+                }
+                if (err.indexOf("email unique") != -1) {
+                    this.isEmailUnique = false;
+                }
             });
     }
 
     ngOnInit(): void {
         this.getRoles();
+        this.buildForm();
     }
-    
+
+    buildForm(): void {
+        const emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+        this.editUserForm = this.fb.group({
+            'firstName': [this.user.firstName, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+            'lastName': [this.user.lastName, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+            'username': [this.user.username, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+            'email': [this.user.email, [Validators.required, Validators.pattern(emailRegex)]],
+            'expirationDate': [this.user.expirationDate],
+            'role': [this.user.role, Validators.required],
+            'canAccessToDicomAssociation': new FormControl('false'),
+            'isMedical': new FormControl('false'),
+            'motivation': [this.user.motivation]
+        });
+
+        this.editUserForm.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+        this.onValueChanged(); // (re)set validation messages now
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.editUserForm) { return; }
+        const form = this.editUserForm;
+        for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                for (const key in control.errors) {
+                    this.formErrors[field] += key;
+                }
+            }
+        }
+    }
+
+    formErrors = {
+        'firstName': '',
+        'lastName': '',
+        'username': '',
+        'email': '',
+        'role': ''
+    };
 }
--- shanoir-ng-front/src/main/webapp/app/users/shared/user.service.ts
+++ shanoir-ng-front/src/main/webapp/app/users/shared/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Response, Headers, Http, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 import { User } from './user.model';
 import * as AppUtils from 'app/utils/app.utils';
@@ -20,20 +21,37 @@ export class UserService {
             .catch((error) => {
                 console.error('Error while getting users', error);
                 return Promise.reject(error.message || error);
-            });
+        });
     }
 
-    create(user: User): Promise<User> {
+    create(user: User): Observable<User> {
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append('x-auth-token', localStorage.getItem(AppUtils.STORAGE_TOKEN));
         
         return this.http.post(AppUtils.BACKEND_API_ROOT_URL + AppUtils.BACKEND_API_CREATE_USER_URL, JSON.stringify(user), new RequestOptions({ headers: headers, withCredentials: true }))
-            .toPromise()
-            .then((res:Response) => res.json())
-            .catch((error) => {
-                console.error('Error while creating users', error);
-                return Promise.reject(error.message || error);
-            });
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || { };
+    }
+
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            errMsg= "[" + body.code + "]: " + body.message;
+            let errDetails = body.details.formErrors || '';
+            for (let errDetail of errDetails) {
+                errMsg += "; " + errDetail.fieldName + " " + errDetail.errorCodes[0];
+            }
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
     }
 }
