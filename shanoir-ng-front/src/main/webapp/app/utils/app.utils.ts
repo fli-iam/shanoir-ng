a0d63f3965d47bbdad0b403b7b613094a961ae8a
yyao <yao.yao@inria.fr>
2016-12-29 16:53:52 +0100

    rest call user url

--- shanoir-ng-front/src/main/webapp/app/utils/app.utils.ts
+++ shanoir-ng-front/src/main/webapp/app/utils/app.utils.ts
@@ -10,6 +10,6 @@ const BACKEND_API_USERS_MS_URL:string = '/users';
 export const BACKEND_API_AUTHENTICATE_PATH:string = BACKEND_API_USERS_MS_URL + '/authenticate';
 export const BACKEND_API_LOGOUT_PATH:string = BACKEND_API_USERS_MS_URL + '/logout';
 export const BACKEND_API_USER_ALL_URL:string = BACKEND_API_USERS_MS_URL + '/user/all';
-export const BACKEND_API_CREATE_USER_URL:string = BACKEND_API_USERS_MS_URL + '/user';
+export const BACKEND_API_USER_URL:string = BACKEND_API_USERS_MS_URL + '/user';
 export const BACKEND_API_ROLE_ALL_URL:string = BACKEND_API_USERS_MS_URL + '/role/all';
 
