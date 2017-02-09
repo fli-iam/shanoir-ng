7ba64963dc5ec25bcfd3a92af903c0a1d24a46ac
Mathieu SIMON <mathieu.simon@inria.fr>
2017-02-09 16:45:29 +0100

    Docker: remove logs

--- shanoir-ng-users/src/main/docker-qualif/docker-entrypoint.sh
+++ shanoir-ng-users/src/main/docker-qualif/docker-entrypoint.sh
@@ -130,27 +130,20 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		fi
 
 		file_env 'MYSQL_DATABASE'
-		echo "MYSQL_DATABASE: $MYSQL_DATABASE"
 		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
 			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
 			mysql+=( "$MYSQL_DATABASE" )
 		fi
 
 		file_env 'MYSQL_USER'
 		file_env 'MYSQL_PASSWORD'
-		echo "MYSQL_USER: $MYSQL_USER"
-		echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
 			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 
 			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;"
 				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
 			fi
 
-			echo 'FLUSH PRIVILEGES ;'
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
 
