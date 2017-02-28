c4b805e397fa33284b137ad24207d1c3a0c8cb7a
yyao <yao.yao@inria.fr>
2017-02-28 10:14:17 +0100

    merge + keycloak config for center ms

--- shanoir-ng-front/src/app/utils/app.utils.ts
+++ shanoir-ng-front/src/app/utils/app.utils.ts
@@ -5,10 +5,10 @@ export const STORAGE_TOKEN_TIMEOUT:string = 'shanoirApp-tokenTimeout';
 export const STORAGE_REFRESH_TOKEN:string = 'shanoirApp-refreshToken';
 
 // Common http root api
-export const BACKEND_API_ROOT_URL:string = 'http://localhost:9901';
+export const BACKEND_API_ROOT_URL:string = 'http://localhost/shanoir-ng';
 
 // Users http api
-const BACKEND_API_USERS_MS_URL:string = '';
+const BACKEND_API_USERS_MS_URL:string = '/users';
 export const BACKEND_API_AUTHENTICATE_PATH:string = BACKEND_API_USERS_MS_URL + '/authenticate';
 export const BACKEND_API_REFRESH_AUTH_TOKEN_PATH:string = BACKEND_API_AUTHENTICATE_PATH + '/token';
 export const BACKEND_API_LOGOUT_PATH:string = BACKEND_API_USERS_MS_URL + '/logout';
@@ -20,6 +20,5 @@ export const BACKEND_API_USER_DENY_ACCOUNT_REQUEST_URL:string = '/denyaccountreq
 export const BACKEND_API_ROLE_ALL_URL:string = BACKEND_API_USERS_MS_URL + '/role/all';
 
 // Centers http api
-// const BACKEND_API_CENTERS_MS_URL: string = '/centers';
-const BACKEND_API_CENTERS_MS_URL: string = '';
+const BACKEND_API_CENTERS_MS_URL: string = '/centers';
 export const BACKEND_API_CENTER_ALL_URL: string = BACKEND_API_CENTERS_MS_URL + '/center/all';
