75f88cc7c72512bdba9120b337a2ae444f60cdc4
Mathieu SIMON <mathieu.simon@inria.fr>
2017-03-28 15:03:19 +0200

    MS studies: bad file name

--- shanoir-ng-studies/src/main/java/org/shanoir/ng/study/StudyApi.java
+++ shanoir-ng-studies/src/main/java/org/shanoir/ng/study/StudyApi.java
@@ -1,24 +1,22 @@
 package org.shanoir.ng.study;
 
-import org.shanoir.ng.shared.exception.ErrorModel;
+import java.util.List;
 
-import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import javax.validation.constraints.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-03-23T10:35:29.288Z")
 
 @Api(value = "study", description = "the study API")
-
 public interface StudyApi {
 
     @ApiOperation(value = "", notes = "Deletes a study", response = Void.class, tags={  })
