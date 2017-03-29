02b422f3d4515ac8f64dafe14ac88cbe877f05b8
Julien Louis <julien.louis@inria.fr>
2017-03-29 10:33:18 +0200

    app store hello world

---
+++ app-store/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="main">
+            <h1>Hello World!</h1>
+        </div>
+    `,
+    directives: [],
+})
+export class AppComponent { 
+    
+} 
