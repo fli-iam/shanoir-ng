ef346e188e9ffae5d3d292b36fe8e1badb848996
Mathieu SIMON <mathieu.simon@inria.fr>
2017-03-30 17:13:53 +0200

    Bug #1153: Hide is medical doctor/resident

--- shanoir-ng-users/src/main/resources/scripts/test-data-h2.sql
+++ shanoir-ng-users/src/main/resources/scripts/test-data-h2.sql
@@ -10,12 +10,12 @@ VALUES
 	(4,200,'Expert','ROLE_EXPERT');
 
 INSERT INTO users
-	(id, account_request_demand, can_access_to_dicom_association, creation_date, email, expiration_date, first_name, is_first_expiration_notification_sent, is_medical, is_second_expiration_notification_sent, last_name, password, username, role_id)
+	(id, account_request_demand, can_access_to_dicom_association, creation_date, email, expiration_date, first_name, is_first_expiration_notification_sent, is_second_expiration_notification_sent, last_name, password, username, role_id)
 VALUES
-	(1, 0, 0, NOW(), 'admin@shanoir.fr', null, 'Michael', 0, 0, 0, 'Kain', 'D0-483351E2-30', 'admin', 1),
-	(2, 0, 0, NOW(), 'jlouis@shanoir.fr', null, 'Julien', 0, 0, 0, 'Louis', '182413194A741C', 'jlouis', 2),
-	(3, 0, 0, NOW(), 'yyao@shanoir.fr', null, 'Yao', 0, 0, 0, 'Yao', '51812A42578709', 'yyao', 2),
-	(4, 0, 0, NOW(), 'jacques.martin@gmail.com', parsedatetime('2016/12/31', 'yyyy/MM/dd'), 'Jacques', 0, 0, 0, 'Martin', '92-1102B43C0-6', 'jmartin', 3),
-	(5, 0, 0, NOW(), 'ricky.martin@gmail.com', null, 'Ricky', 0, 0, 0, 'Martin', '4165C7-57B4-76', 'wopa', 3),
-	(6, 0, 0, NOW(), 'michel.sardou@gmail.com', null, 'Michel', 0, 0, 0, 'sardou', '1117DC-36DE-34', 'connemara', 3),
-	(7, 0, 0, NOW(), 'paul.bismuth@gmail.com', null, 'Paul', 0, 0, 0, 'Bismuth', 'F8-820327C124B', 'ns2017', 4);
+	(1, 0, 0, NOW(), 'admin@shanoir.fr', null, 'Michael', 0, 0, 'Kain', 'D0-483351E2-30', 'admin', 1),
+	(2, 0, 0, NOW(), 'jlouis@shanoir.fr', null, 'Julien', 0, 0, 'Louis', '182413194A741C', 'jlouis', 2),
+	(3, 0, 0, NOW(), 'yyao@shanoir.fr', null, 'Yao', 0, 0, 'Yao', '51812A42578709', 'yyao', 2),
+	(4, 0, 0, NOW(), 'jacques.martin@gmail.com', parsedatetime('2016/12/31', 'yyyy/MM/dd'), 'Jacques', 0, 0, 'Martin', '92-1102B43C0-6', 'jmartin', 3),
+	(5, 0, 0, NOW(), 'ricky.martin@gmail.com', null, 'Ricky', 0, 0, 'Martin', '4165C7-57B4-76', 'wopa', 3),
+	(6, 0, 0, NOW(), 'michel.sardou@gmail.com', null, 'Michel', 0, 0, 'sardou', '1117DC-36DE-34', 'connemara', 3),
+	(7, 0, 0, NOW(), 'paul.bismuth@gmail.com', null, 'Paul', 0, 0, 'Bismuth', 'F8-820327C124B', 'ns2017', 4);
