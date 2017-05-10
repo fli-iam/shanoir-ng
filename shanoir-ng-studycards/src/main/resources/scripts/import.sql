7ecd13d300257f8d0e43d9e5197a9995aa1eef35
Mathieu SIMON <mathieu.simon@inria.fr>
2017-05-10 17:40:47 +0200

    MS study cards: migration file

--- shanoir-ng-studycards/src/main/resources/scripts/import.sql
+++ shanoir-ng-studycards/src/main/resources/scripts/import.sql
@@ -4,9 +4,10 @@
 
 --use shanoir_ng_studyCards;
 
-INSERT INTO study_cards (id, disabled, name, study_id)
+INSERT INTO study_cards 
+	(id, acquisition_equipment_id, center_id, disabled, name, nifti_converter_id, study_id)
 VALUES 
-	(1 ,0 ,'StudyCard1', 1),
-	(2 ,0 ,'StudyCard2', 1),
-	(3 ,0 ,'StudyCard3', 2),
-	(4 ,0 ,'StudyCard4', 3);
+	(1, 1, 1, 0, 'StudyCard1', 1, 1),
+	(2, 1, 1, 0, 'StudyCard2', 1, 1),
+	(3, 3, 1, 0, 'StudyCard3', 1, 2),
+	(4, 4, 3, 0, 'StudyCard4', 1, 3);
