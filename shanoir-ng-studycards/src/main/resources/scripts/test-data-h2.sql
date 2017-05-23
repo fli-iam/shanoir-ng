2f315deff3812d7c846da03ac44fe6a5a6505e3a
Mathieu SIMON <mathieu.simon@inria.fr>
2017-05-23 10:47:36 +0200

    UT updated

--- shanoir-ng-studycards/src/main/resources/scripts/test-data-h2.sql
+++ shanoir-ng-studycards/src/main/resources/scripts/test-data-h2.sql
@@ -1,8 +1,9 @@
 -- Populates database for test
 
-INSERT INTO study_cards (id, disabled, name, study_id)
+INSERT INTO study_cards 
+	(id, acquisition_equipment_id, center_id, disabled, name, nifti_converter_id, study_id)
 VALUES 
-	(1 ,0 ,'StudyCard1', 1),
-	(2 ,0 ,'StudyCard2', 1),
-	(3 ,0 ,'StudyCard3', 2),
-	(4 ,0 ,'StudyCard4', 3);
+	(1, 1, 1, 0, 'StudyCard1', 1, 1),
+	(2, 1, 1, 0, 'StudyCard2', 1, 1),
+	(3, 3, 1, 0, 'StudyCard3', 1, 2),
+	(4, 4, 3, 0, 'StudyCard4', 1, 3);
