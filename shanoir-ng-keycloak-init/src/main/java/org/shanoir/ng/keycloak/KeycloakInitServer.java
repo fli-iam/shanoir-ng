80b4012cbb7478cdd345cfe6aa54c1410e33267f
Mathieu SIMON <mathieu.simon@inria.fr>
2017-05-23 15:15:19 +0200

    Qualif - Keycloak initialization: front client updated with env var

--- shanoir-ng-keycloak-init/src/main/java/org/shanoir/ng/keycloak/KeycloakInitServer.java
+++ shanoir-ng-keycloak-init/src/main/java/org/shanoir/ng/keycloak/KeycloakInitServer.java
@@ -1,5 +1,6 @@
 package org.shanoir.ng.keycloak;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +9,13 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.BasicConfigurator;
 import org.keycloak.admin.client.resource.AuthenticationManagementResource;
+import org.keycloak.admin.client.resource.ClientResource;
+import org.keycloak.admin.client.resource.ClientsResource;
 import org.keycloak.representations.idm.AuthenticationExecutionInfoRepresentation;
 import org.keycloak.representations.idm.AuthenticationExecutionRepresentation;
 import org.keycloak.representations.idm.AuthenticationFlowRepresentation;
 import org.keycloak.representations.idm.AuthenticatorConfigRepresentation;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.RequiredActionProviderRepresentation;
 import org.slf4j.Logger;
@@ -21,35 +25,60 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class KeycloakInitServer extends AbstractKeycloakInit
-{
+public class KeycloakInitServer extends AbstractKeycloakInit {
+
 	/**
 	 * Logger
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(KeycloakInitServer.class);
 
+	private static final String BASE_URL_ENV = "BASE_URL";
 	private static final String BROWSER_FLOW = "browser";
+	private static final String FRONT_CLIENT_ID = "shanoir-ng-front";
 	private static final String NEW_BROWSER_FLOW = "Browser script";
 	private static final String NEW_EXECUTION_CONFIG_NAME = "CheckExpirationDateConfig";
 	private static final String REQUIRED_ACTION_NAME = "Record Login Date Action";
-	
-    public static void main( String[] args ) {
-    	BasicConfigurator.configure();
-    	
-    	loadParams();
+
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+
+		loadParams();
+		updateFrontClient();
 		createAuthenticationFlow();
 		updateRealm();
 		updateRequiredAction();
 	}
-	
+
+	/*
+	 * Update front client: set URLs from environment variables
+	 */
+	private static void updateFrontClient() {
+		LOG.info("Update front client");
+
+		final String baseUrl = System.getenv(BASE_URL_ENV);
+
+		if (baseUrl != null && !baseUrl.contains("localhost")) {
+			final ClientsResource clientsResource = getKeycloak().realm(getKeycloakRealm()).clients();
+			final List<ClientRepresentation> clientRepresentations = clientsResource.findByClientId(FRONT_CLIENT_ID);
+			if (clientRepresentations != null && !clientRepresentations.isEmpty()) {
+				final ClientRepresentation clientRepresentation = clientRepresentations.get(0);
+				clientRepresentation.setRedirectUris(Arrays.asList(baseUrl + "/*"));
+				clientRepresentation.setWebOrigins(Arrays.asList(baseUrl));
+				final ClientResource clientResource = clientsResource.get(clientRepresentation.getId());
+				clientResource.update(clientRepresentation);
+			}
+		}
+	}
+
 	/*
 	 * Create authentication flow
 	 */
 	private static void createAuthenticationFlow() {
 		LOG.info("Create authentication flow");
-		
-		final AuthenticationManagementResource authenticationManagement = getKeycloak().realm(getKeycloakRealm()).flows();
-		
+
+		final AuthenticationManagementResource authenticationManagement = getKeycloak().realm(getKeycloakRealm())
+				.flows();
+
 		// Create new flow
 		LOG.info("Create new flow");
 		Map<String, String> data = new HashMap<>();
@@ -58,7 +87,7 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 		if (response.getStatus() != 201) {
 			LOG.error("Error on flow copy");
 		}
-		
+
 		// Get flow id
 		LOG.info("Get flow id");
 		final List<AuthenticationFlowRepresentation> flows = authenticationManagement.getFlows();
@@ -69,7 +98,7 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 				break;
 			}
 		}
-		
+
 		// Create new execution (provider: Script)
 		LOG.info("Create new execution (provider: Script)");
 		final AuthenticationExecutionRepresentation authenticationExecution = new AuthenticationExecutionRepresentation();
@@ -80,10 +109,11 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 		if (response.getStatus() != 201) {
 			LOG.error("Error on execution creation");
 		}
-		
+
 		// Get execution id
 		LOG.info("Get execution id");
-		final List<AuthenticationExecutionInfoRepresentation> exeutions = authenticationManagement.getExecutions(NEW_BROWSER_FLOW);
+		final List<AuthenticationExecutionInfoRepresentation> exeutions = authenticationManagement
+				.getExecutions(NEW_BROWSER_FLOW);
 		AuthenticationExecutionInfoRepresentation executionInfo = null;
 		for (AuthenticationExecutionInfoRepresentation execution : exeutions) {
 			if ("Script".equals(execution.getDisplayName())) {
@@ -91,17 +121,19 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 				break;
 			}
 		}
-		
+
 		// Create new execution config
 		LOG.info("Create new execution config");
 		final Map<String, String> config = new HashMap<>();
-		config.put("scriptCode", "/*\n * Template for JavaScript based authenticator\'s.\n * See org.keycloak.authentication.authenticators.browser.ScriptBasedAuthenticatorFactory\n */\n\n// import enum for error lookup\n//AuthenticationFlowError = Java.type(\"org.keycloak.authentication.AuthenticationFlowError\");\n\n/**\n * An example authenticate function.\n *\n * The following variables are available for convenience:\n * user - current user {@see org.keycloak.models.UserModel}\n * realm - current realm {@see org.keycloak.models.RealmModel}\n * session - current KeycloakSession {@see org.keycloak.models.KeycloakSession}\n * httpRequest - current HttpRequest {@see org.jboss.resteasy.spi.HttpRequest}\n * script - current script {@see org.keycloak.models.ScriptModel}\n * LOG - current logger {@see org.jboss.logging.Logger}\n *\n * You one can extract current http request headers via:\n * httpRequest.getHttpHeaders().getHeaderString(\"Forwarded\")\n *\n * @param context {@see org.keycloak.authentication.AuthenticationFlowContext}\n */\nfunction authenticate(context) {\n\n    var username = user ? user.username : \"anonymous\";\n    LOG.info(script.name + \" trace auth for: \" + username);\n\n    var authShouldFail = false;\n    if (authShouldFail) {\n        context.failure(\"INVALID_USER\");\n        return;\n    }\n    \n    if (user.attributes[\'expirationDate\'] !== null && new Date().getTime() > user.attributes[\'expirationDate\'][0]) {\n        user.enabled = false;\n        context.failure(\"USER_DISABLED\");\n        return;\n    }\n\n    context.success();\n}");
+		config.put("scriptCode",
+				"/*\n * Template for JavaScript based authenticator\'s.\n * See org.keycloak.authentication.authenticators.browser.ScriptBasedAuthenticatorFactory\n */\n\n// import enum for error lookup\n//AuthenticationFlowError = Java.type(\"org.keycloak.authentication.AuthenticationFlowError\");\n\n/**\n * An example authenticate function.\n *\n * The following variables are available for convenience:\n * user - current user {@see org.keycloak.models.UserModel}\n * realm - current realm {@see org.keycloak.models.RealmModel}\n * session - current KeycloakSession {@see org.keycloak.models.KeycloakSession}\n * httpRequest - current HttpRequest {@see org.jboss.resteasy.spi.HttpRequest}\n * script - current script {@see org.keycloak.models.ScriptModel}\n * LOG - current logger {@see org.jboss.logging.Logger}\n *\n * You one can extract current http request headers via:\n * httpRequest.getHttpHeaders().getHeaderString(\"Forwarded\")\n *\n * @param context {@see org.keycloak.authentication.AuthenticationFlowContext}\n */\nfunction authenticate(context) {\n\n    var username = user ? user.username : \"anonymous\";\n    LOG.info(script.name + \" trace auth for: \" + username);\n\n    var authShouldFail = false;\n    if (authShouldFail) {\n        context.failure(\"INVALID_USER\");\n        return;\n    }\n    \n    if (user.attributes[\'expirationDate\'] !== null && new Date().getTime() > user.attributes[\'expirationDate\'][0]) {\n        user.enabled = false;\n        context.failure(\"USER_DISABLED\");\n        return;\n    }\n\n    context.success();\n}");
 		config.put("scriptDescription", "Check expiration date on login");
 		config.put("scriptName", "CheckExpirationDate");
 		final AuthenticatorConfigRepresentation authenticatorConfig = new AuthenticatorConfigRepresentation();
 		authenticatorConfig.setAlias(NEW_EXECUTION_CONFIG_NAME);
 		authenticatorConfig.setConfig(config);
-		response = getKeycloak().realm(getKeycloakRealm()).flows().newExecutionConfig(executionInfo.getId(), authenticatorConfig);
+		response = getKeycloak().realm(getKeycloakRealm()).flows().newExecutionConfig(executionInfo.getId(),
+				authenticatorConfig);
 		if (response.getStatus() != 201) {
 			LOG.error("Error on execution config creation");
 		}
@@ -120,12 +152,13 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 		realm.setResetPasswordAllowed(Boolean.TRUE);
 		getKeycloak().realm(getKeycloakRealm()).update(realm);
 	}
-	
+
 	/*
 	 * Update required action
 	 */
 	private static void updateRequiredAction() {
-		final List<RequiredActionProviderRepresentation> requiredActions = getKeycloak().realm(getKeycloakRealm()).flows().getRequiredActions();
+		final List<RequiredActionProviderRepresentation> requiredActions = getKeycloak().realm(getKeycloakRealm())
+				.flows().getRequiredActions();
 		for (RequiredActionProviderRepresentation action : requiredActions) {
 			if (REQUIRED_ACTION_NAME.equals(action.getAlias())) {
 				action.setEnabled(true);
@@ -135,5 +168,5 @@ public class KeycloakInitServer extends AbstractKeycloakInit
 			}
 		}
 	}
-	
+
 }
