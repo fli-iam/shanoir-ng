c6ce037969aef73442a2c35f64f7e7e99d169ce9
yyao <yao.yao@inria.fr>
2017-05-30 16:15:58 +0200

    manufacturer model

--- shanoir-ng-front/src/app/acqEquip/manufModel/detail/manufModel.detail.component.ts
+++ shanoir-ng-front/src/app/acqEquip/manufModel/detail/manufModel.detail.component.ts
@@ -5,7 +5,9 @@ import { Observable } from 'rxjs/Observable';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 import { ManufacturerModel } from '../../shared/manufModel.model';
-import { AcquisitionEquipmentService } from '../../shared/acqEquip.service';
+import { ManufacturerModelService } from '../../shared/manufModel.service';
+import { Manufacturer } from '../../shared/manuf.model';
+import { ManufacturerService } from '../../shared/manuf.service';
 import { KeycloakService } from "../../../shared/keycloak/keycloak.service";
 import { DatasetModalityType } from "../../../shared/enum/datasetModalityType";
 import { Enum } from "../../../shared/utils/enum";
@@ -17,8 +19,6 @@ import { Enum } from "../../../shared/utils/enum";
 
 export class ManufacturerModelDetailComponent implements OnInit {
     
-    @Output() closing = new EventEmitter();
-    @Input() modeFromCenterList: "view" | "edit" | "create";
     private manufModel: ManufacturerModel = new ManufacturerModel();
     private manufModelDetailForm: FormGroup;
     private manufModelId: number;
@@ -26,17 +26,20 @@ export class ManufacturerModelDetailComponent implements OnInit {
     private isNameUnique: Boolean = true;
     private canModify: Boolean = false;
     private datasetModalityTypes: Enum[] = []; 
+    private isMR: Boolean = false;
+    private manufs: Manufacturer[];
 
     constructor (private route: ActivatedRoute, private router: Router,
-        private acqEquipService: AcquisitionEquipmentService,   private fb: FormBuilder,
-        private location: Location, private keycloakService: KeycloakService) {
+        private manufModelService: ManufacturerModelService, private manufService: ManufacturerService,   
+        private fb: FormBuilder, private location: Location, 
+        private keycloakService: KeycloakService) {
 
     }
 
     ngOnInit(): void {
-        if (this.modeFromCenterList) {this.mode = this.modeFromCenterList;}
         this.getEnum();
-        // this.getManufacturerModel();
+        this.getManufs();
+        this.getManufacturerModel();
         this.buildForm();
         if (this.keycloakService.isUserAdmin() || this.keycloakService.isUserExpert()) {
             this.canModify = true;
@@ -53,33 +56,52 @@ export class ManufacturerModelDetailComponent implements OnInit {
         }
     }
 
-    // getManufacturerModel(): void {
-    //     this.route.queryParams
-    //         .switchMap((queryParams: Params) => {
-    //             let manufModelId = queryParams['id'];
-    //             let mode = queryParams['mode'];
-    //             if (mode) {
-    //                 this.mode = mode;
-    //             }
-    //             if (manufModelId) {
-    //                 // view or edit mode
-    //                 this.manufModelId = manufModelId;
-    //                 return this.acqEquipService.getManufacturerModel(manufModelId);
-    //             } else { 
-    //                 // create mode
-    //                 return Observable.of<ManufacturerModel>();
-    //             }
-    //         })
-    //         .subscribe((manufModel: ManufacturerModel) => {
-    //             this.manufModel = manufModel;
-    //         });
-    // }   
+    getManufs(): void {
+    this.manufService
+        .getManufacturers()
+        .then(manufs => {
+            this.manufs = manufs;
+        })
+        .catch((error) => {
+            // TODO: display error
+            console.log("error getting manufacturer list!");
+        });
+    }
+
+    getManufacturerModel(): void {
+        this.route.queryParams
+            .switchMap((queryParams: Params) => {
+                let manufModelId = queryParams['id'];
+                let mode = queryParams['mode'];
+                if (mode) {
+                    this.mode = mode;
+                }
+                if (manufModelId) {
+                    // view or edit mode
+                    this.manufModelId = manufModelId;
+                    return this.manufModelService.getManufacturerModel(manufModelId);
+                } else { 
+                    // create mode
+                    return Observable.of<ManufacturerModel>();
+                }
+            })
+            .subscribe((manufModel: ManufacturerModel) => {
+                this.manufModel = manufModel;
+            });
+    }   
 
     buildForm(): void {
+        let magneticFieldFC: FormControl;
+        if (this.isMR) {
+            magneticFieldFC = new FormControl('this.manufModel.magneticField', Validators.required);
+        } else {
+            magneticFieldFC = new FormControl('this.manufModel.magneticField');
+        }
         this.manufModelDetailForm = this.fb.group({
-            'name': [this.manufModel.name],
-            'magneticField': [this.manufModel.magneticField],
-            'datasetModalityType': [this.manufModel.datasetModalityType]
+            'name': [this.manufModel.name, [Validators.required, Validators.minLength(2), Validators.maxLength(200)]],
+            'manufacturer': [this.manufModel.manufacturer, Validators.required],
+            'magneticField': magneticFieldFC,
+            'datasetModalityType': [this.manufModel.datasetModalityType, Validators.required]
         });
         this.manufModelDetailForm.valueChanges
             .subscribe(data => this.onValueChanged(data));
@@ -101,49 +123,67 @@ export class ManufacturerModelDetailComponent implements OnInit {
         }
     }
 
+    onSelect(datasetModalityType) {
+        if (datasetModalityType.indexOf("MR") != -1) {
+            this.isMR = true;
+        } else {
+            this.isMR = false;
+        }
+         this.buildForm();
+    }
+
     formErrors = {
-        'name': ''
+        'manufacturer': '',
+        'name': '',
+        'datasetModalityType': '',
+        'magneticField': ''
     };
 
     back(): void {
-        //this.location.back();
-        this.getOut();
+        this.location.back();
+        // this.getOut();
     }
 
     edit(): void {
         this.router.navigate(['/manufModelDetail'], { queryParams: {id: this.manufModelId, mode: "edit"}});
     }
 
-    // create(): void {
-    //     this.manufModel = this.manufModelDetailForm.value;
-    //     this.acqEquipService.create(this.manufModel)
-    //     .subscribe((manufModel) => {
-    //         this.getOut();
-    //     }, (err: String) => {
-    //         if (err.indexOf("name should be unique") != -1) {
-    //             this.isNameUnique = false;
-    //         }
-    //     });
-    // }
-
-    // update(): void {
-    //     this.manufModel = this.manufModelDetailForm.value;
-    //     this.acqEquipService.update(this.manufModelId, this.manufModel)
-    //     .subscribe((manufModel) => {
-    //         this.getOut();
-    //     }, (err: String) => {
-    //         if (err.indexOf("name should be unique") != -1) {
-    //             this.isNameUnique = false;
-    //         }
-    //     });
-    // }
-
-    getOut(manufModel: ManufacturerModel = null): void {
-        if (this.closing.observers.length > 0) {
-            this.closing.emit(manufModel);
-        } else {
-            this.location.back();
+    create(): void {
+        this.manufModel = this.manufModelDetailForm.value;
+        if (!this.isMR) {
+            this.manufModel.magneticField = null;
         }
+        this.manufModelService.create(this.manufModel)
+        .subscribe((manufModel) => {
+            this.back();
+        }, (err: String) => {
+            if (err.indexOf("name should be unique") != -1) {
+                this.isNameUnique = false;
+            }
+        });
     }
+
+    update(): void {
+        this.manufModel = this.manufModelDetailForm.value;
+        if (!this.isMR) {
+            this.manufModel.magneticField = null;
+        }
+        this.manufModelService.update(this.manufModelId, this.manufModel)
+        .subscribe((manufModel) => {
+            this.back();
+        }, (err: String) => {
+            if (err.indexOf("name should be unique") != -1) {
+                this.isNameUnique = false;
+            }
+        });
+    }
+
+    // getOut(manufModel: ManufacturerModel = null): void {
+    //     if (this.closing.observers.length > 0) {
+    //         this.closing.emit(manufModel);
+    //     } else {
+    //         this.location.back();
+    //     }
+    // }
 
 }
