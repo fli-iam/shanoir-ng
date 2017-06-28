ca945db575f9d7b2f1e1321cd2178dd324a94a90
Anthony Baire <Anthony.Baire@irisa.fr>
2017-06-28 15:55:49 +0200

    configure supervisor in the template image directly
    + provide a run-supervisor script to run it (and which creates
      /vol/log/supervisor on-the-fly)

--- shanoir-ng-users/src/main/docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "/vol/log/supervisor" ]; then
-  echo "Create supervisor log directory"
-  mkdir -p /vol/log/supervisor
-fi
-
-supervisord -c /etc/supervisor/supervisord.conf
