e1928ccbc5365d9113fb83e65f15b1a146842c7c
Anthony Baire <Anthony.Baire@irisa.fr>
2017-06-28 15:51:57 +0200

    refactored the handling of the mariadb server
    - use the mariadb package provided by debian
      - use debian's default scripts
      - use debian's default config (and tune it with a patch instead of
        copying the files)
    - move all mysql stuff into the template image and provide admin scripts
      to be used in the derived images:
      - init-mysql  -> intialise the mysql db
      - run-mysql   -> run the mysql server
      - wait-mysql  -> wait until the mysql server is ready
      - mysql.conf  -> supervisor config (put in template.d/ to keep it idle,
                       to be copied into conf.d/ to be enabled)
    - remove the overly complex docker-entrypoint.sh (from the mariadb image)
      and use init-mysql instead (which uses mysql_install_db script provided
      by debian)
    - introduce the MS (microservice name) and PROFILE (configuration profile)
      environment variables to standardise the images:
      - mysql db name derived from the value of MS
      - container_init.{dev,qualif} merged into a single script that expects
        PROFILE to be set
    - run mysqld from the 'run-mysql' script which also creates the required
      directories (/vol/log/mysql ...)
    - use « set -e » (abort on error) on all initialisation scripts to ensure
      all errors are caught
    - put the sql script into /dk/data/ so that the dk script does not issue
      warnings about an unknown script

--- shanoir-ng-users/src/main/docker/run.sh
+++ shanoir-ng-users/src/main/docker/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "/var/run/mysqld/mysqld.sock" ]; then
-  echo "Initialize MariaDB command"
-  /opt/mysql/init/mariadb_init.sh mysqld
-fi
-
 if [ ! -d "/vol/log/supervisor" ]; then
   echo "Create supervisor log directory"
   mkdir -p /vol/log/supervisor
