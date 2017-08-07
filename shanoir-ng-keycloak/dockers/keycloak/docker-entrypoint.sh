59c5fe784cebcc33176d18fce573a0f927d4e9f2
Anthony Baire <Anthony.Baire@irisa.fr>
2017-08-07 14:55:05 +0200

    select the theme at runtime

--- shanoir-ng-keycloak/dockers/keycloak/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /opt/jboss/keycloak/themes/shanoir-theme/login/theme-$DOCKER_PROFILE.properties /opt/jboss/keycloak/themes/shanoir-theme/login/theme.properties
-
-exec /opt/jboss/keycloak/bin/standalone.sh $@
-exit $?
