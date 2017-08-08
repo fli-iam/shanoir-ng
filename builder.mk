6aa7ce9d5fae8fc2f014920a1b8dcd9417b4dfb6
Anthony Baire <Anthony.Baire@irisa.fr>
2017-08-08 17:11:26 +0200

    disable studycards & studies

--- builder.mk
+++ builder.mk
@@ -4,6 +4,8 @@
 
 # designed to build all images from scratch
 
+#TODO: enable studies & studycards
+MICROSERVICES = users #studies studycards
 
 # keycloak images
 #
@@ -30,18 +32,19 @@ base-ms-image: base-image
 	docker build -t shanoir-ng/base-ms shanoir-ng-template/shanoir-ng-base-ms
 
 # microservices
-users studies studycards: %: base-ms-image
+$(MICROSERVICES): %: base-ms-image
 	# https://github.com/fli-iam/shanoir-ng/wiki/Installation-guide-4%29-Docker-shanoir-ng-users
 	(cd 'shanoir-ng-$@/' && mvn clean package docker:build -DskipTests -Pqualif)
 
 # shanoir-ng-nginx
 #
-# https://github.com/fli-iam/shanoir-ng/wiki/Installation-guide-6%29-Docker-Nginx-with-statics
-nginx: keycloak users studies studycards
+# https://github.com/fli-iam/shanoir-ng/wiki/Installation-guide-6%29-Docker-Nginx-with-staticsa
+#nginx: keycloak users studies studycards
+nginx: keycloak users
 	npm set registry https://registry.npmjs.org
 
 	(cd shanoir-ng-front && mvn package -Pqualif)
 	(cd shanoir-ng-nginx && docker build -t shanoir-ng/nginx:latest .)
 
 # all images for shanoir NG
-all: keycloak users studies studycards nginx
+all: keycloak $(MICROSERVICES) nginx
