cc5932973689efd9a81048d5eb8dba36e0d23e07
Math <mathieu.simon@inria.fr>
2018-05-30 09:37:42 +0200

    Front: set application/json value content-type for private and public URL

--- shanoir-ng-front/src/app/shared/keycloak/keycloak.http.interceptor.ts
+++ shanoir-ng-front/src/app/shared/keycloak/keycloak.http.interceptor.ts
@@ -12,21 +12,21 @@ import { KeycloakService } from "./keycloak.service";
 export class KeycloakHttpInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.url.endsWith('/accountrequest')) {
-            // Public URL. No bearer needed
-            return next.handle(req);
-        }
-        // Get the auth header from the service.
-        const authHeader = KeycloakService.auth.authz.token;
-        // Clone the request to add the new header.
-        let authReq = req.clone({
-            setHeaders: {
-                Authorization: `Bearer ${authHeader}`
-            }
-        });
+        let authReq: HttpRequest<any>;
         // Do not add Content-Type application/json for Form Data
         if (!(req.body instanceof FormData)) {
-            authReq = authReq.clone({ headers: authReq.headers.set('Content-Type', 'application/json') });
+            authReq = req.clone({ headers: authReq.headers.set('Content-Type', 'application/json') });
+        }
+        // Bearer needed for private URL only (".../accountrequest" is a public URL)
+        if (!authReq.url.endsWith('/accountrequest')) {
+            // Get the auth header from the service.
+            const authHeader = KeycloakService.auth.authz.token;
+            // Clone the request to add the new header.
+            authReq = req.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${authHeader}`
+                }
+            });
         }
         // Pass on the cloned request instead of the original request.
         return next.handle(authReq);
