6c0542f33f7555e5af38385dc4f45e55e90ab303
atouboul <arnaud.touboulic@irisa.fr>
2018-06-18 16:27:49 +0200

    fix if serie selected during import

--- shanoir-ng-import/src/main/java/org/shanoir/ng/importer/ImporterApiController.java
+++ shanoir-ng-import/src/main/java/org/shanoir/ng/importer/ImporterApiController.java
@@ -34,6 +34,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -137,7 +138,9 @@ public class ImporterApiController implements ImporterApi {
 			LOG.info(importJobJsonString);
 			
 			// HttpEntity represents the request
-			final HttpEntity<ImportJob> requestBody = new HttpEntity<>(importJob, KeycloakUtil.getKeycloakHeader());
+			HttpHeaders headers = new HttpHeaders();
+
+			final HttpEntity<ImportJob> requestBody = new HttpEntity<>(importJob, headers);
 
 			// Post to dataset MS to finish import
 			ResponseEntity<String> response = null;
