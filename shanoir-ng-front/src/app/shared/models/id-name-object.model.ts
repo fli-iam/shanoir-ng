29d09ed8c9a52992f7e2eddbfbcd847f36b6848b
Julien Louis <jul.louis@gmail.com>
2018-09-06 11:00:00 +0200

    id name object sort specification

--- shanoir-ng-front/src/app/shared/models/id-name-object.model.ts
+++ shanoir-ng-front/src/app/shared/models/id-name-object.model.ts
@@ -4,4 +4,16 @@ export class IdNameObject {
         public name: string, 
         public selected: boolean = false
     ) {}
+
+    public static sortAll(objects: IdNameObject[], on: 'id' | 'name'): void {
+        objects.sort((inoA, inoB) => {
+
+            // If the return number is negative, A will be shown before B.
+            // If the return number is 0, A and B will remain in the same order as when they entered the loop.
+            // If the return number is positive, B will be shown before A.
+
+            return 0;
+
+        });
+    }
 }
