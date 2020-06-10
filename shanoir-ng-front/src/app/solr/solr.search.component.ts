220c564dca185aea670c08644b1b47d57060862b
yyao1231 <yao.yao@inria.fr>
2020-06-10 14:24:47 +0200

    resize column

--- shanoir-ng-front/src/app/solr/solr.search.component.ts
+++ shanoir-ng-front/src/app/solr/solr.search.component.ts
@@ -171,7 +171,7 @@ export class SolrSearchComponent{
         };
 
         let columnDefs: any = [
-            {headerName: "Id", field: "id", type: "number", width: "30px", defaultSortCol: true, defaultAsc: false},
+            {headerName: "Id", field: "id", type: "number", width: "60px", defaultSortCol: true, defaultAsc: false},
             {headerName: "Name", field: "datasetName"},
             {headerName: "Type", field: "datasetType", width: "30px"},
             {headerName: "Nature", field: "datasetNature", width: "30px"},
