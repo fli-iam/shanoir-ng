b4a258c9976efbadc37d3d88ea9cd60427dab88b
Julien Louis <jul.louis@gmail.com>
2020-09-25 11:39:35 +0200

    un-disable save button when update extra data

--- shanoir-ng-front/src/app/examinations/examination/examination.component.ts
+++ shanoir-ng-front/src/app/examinations/examination/examination.component.ts
@@ -157,12 +157,16 @@ export class ExaminationComponent extends EntityComponent<Examination> {
     protected deleteFile(file: any) {
         this.examination.extraDataFilePathList = this.examination.extraDataFilePathList.filter(fileToKeep => fileToKeep != file);
         this.files = this.files.filter(fileToKeep => fileToKeep.name != file);
+	this.form.markAsDirty();
+        this.form.updateValueAndValidity();
     }
 
     private attachNewFile(event: any) {
         let newFile = event.target.files[0];
         this.examination.extraDataFilePathList.push(newFile.name);
         this.files.push(newFile);
+	this.form.markAsDirty();
+        this.form.updateValueAndValidity();
     }
 
     protected save(): Promise<void> {
