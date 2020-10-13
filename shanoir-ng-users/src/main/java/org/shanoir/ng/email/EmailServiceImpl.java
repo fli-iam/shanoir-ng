57f1fa4c53fb34f55a2da38de96742c5dc2ad2a3
JcomeD <jean-come.douteau@univ-grenoble-alpes.fr>
2020-10-13 09:20:29 +0200

    Shanoir-issue#581 Account request
    - Change email to adapted template

--- shanoir-ng-users/src/main/java/org/shanoir/ng/email/EmailServiceImpl.java
+++ shanoir-ng-users/src/main/java/org/shanoir/ng/email/EmailServiceImpl.java
@@ -23,7 +23,6 @@ import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.shanoir.ng.events.ShanoirEvent;
-import org.shanoir.ng.shared.configuration.RabbitMQConfiguration;
 import org.shanoir.ng.user.model.User;
 import org.shanoir.ng.user.repository.UserRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -174,7 +173,7 @@ public class EmailServiceImpl implements EmailService {
 			variables.put(LASTNAME, user.getLastName());
 			variables.put("password", password);
 			variables.put("username", user.getUsername());
-			final String content = build("notifyNewUser", variables);
+			final String content = build("notifyCreateAccountRequest", variables);
 			messageHelper.setText(content, true);
 		};
 		mailSender.send(messagePreparator);
