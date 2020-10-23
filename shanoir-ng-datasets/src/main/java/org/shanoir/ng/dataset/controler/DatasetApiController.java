6d5c4e5b3b715df8e40d8075aa8b9069850fe658
JcomeD <jean-come.douteau@univ-grenoble-alpes.fr>
2020-10-23 14:55:24 +0200

    Shanoir-issue#617 Nitfi file in dicom
    - Download DICOM only (change download name with date)
    - Do not load nifti everytime => Only on demand
    - Exception management

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/controler/DatasetApiController.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/controler/DatasetApiController.java
@@ -263,7 +263,9 @@ public class DatasetApiController implements DatasetApi {
 		}
 
 		String tmpFilePath = userDir + File.separator + datasetName;
-		File workFolder = new File(tmpFilePath + DOWNLOAD);
+
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+		File workFolder = new File(tmpFilePath + "-" + formatter.format(new DateTime().toDate()) + DOWNLOAD);
 		workFolder.mkdirs();
 		File zipFile = new File(tmpFilePath + ZIP);
 		zipFile.createNewFile();
@@ -284,10 +286,12 @@ public class DatasetApiController implements DatasetApi {
 						new ErrorModel(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Bad arguments", null));
 			}
 		} catch (IOException | MessagingException e) {
+			LOG.error("Error while retrieveing dataset data.", e);
 			FileUtils.deleteQuietly(workFolder);
+			FileUtils.deleteQuietly(zipFile);
 
 			throw new RestServiceException(
-					new ErrorModel(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Error in WADORSDownloader.", e.getLocalizedMessage()));
+					new ErrorModel(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Error while retrieveing dataset data.", e));
 		}
 		zip(workFolder.getAbsolutePath(), zipFile.getAbsolutePath());
 
@@ -296,6 +300,8 @@ public class DatasetApiController implements DatasetApi {
 
 		byte[] data = Files.readAllBytes(zipFile.toPath());
 		ByteArrayResource resource = new ByteArrayResource(data);
+		
+		FileUtils.deleteQuietly(workFolder);
 
 		return ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION, ATTACHMENT_FILENAME + zipFile.getName())
