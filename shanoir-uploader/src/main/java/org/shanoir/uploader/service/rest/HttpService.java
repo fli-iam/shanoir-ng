bb67aa6f538f77601608b99cb66f6eaf04e0f908
Michael Kain <michael.kain@inria.fr>
2020-10-30 16:39:45 +0100

    Change wrong import.

--- shanoir-uploader/src/main/java/org/shanoir/uploader/service/rest/HttpService.java
+++ shanoir-uploader/src/main/java/org/shanoir/uploader/service/rest/HttpService.java
@@ -28,7 +28,6 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.shanoir.uploader.ShUpOnloadConfig;
-import org.shanoir.uploader.service.soap.ServiceConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
