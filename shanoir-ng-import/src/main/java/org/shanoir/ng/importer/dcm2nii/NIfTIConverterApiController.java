8cca4b428a8eb1ef2c6f4488a63a5d27c50600f1
jeancome.douteau <jean-come.douteau@univ-grenoble-alpes.fr>
2021-04-20 12:27:36 +0200

    shanoir-issue#690 tests

--- shanoir-ng-import/src/main/java/org/shanoir/ng/importer/dcm2nii/NIfTIConverterApiController.java
+++ shanoir-ng-import/src/main/java/org/shanoir/ng/importer/dcm2nii/NIfTIConverterApiController.java
@@ -56,9 +56,4 @@ public class NIfTIConverterApiController implements NIfTIConverterApi{
 		return new ResponseEntity<>(niftiConverters, HttpStatus.OK);
 	}
 
-	@Override
-	public ResponseEntity<Boolean> convertData(Long niftiConverterId, String dataPath) {
-		
-		return null;
-	}
 }
