313668778160f3185a24e503bbf3b71b91d14316
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2021-05-17 17:25:17 +0200

    Shanoir-hotfix#947 Correct access to old datasets

--- db-changes/datasets/17052021_old_datasets_access.sql
+++ db-changes/datasets/17052021_old_datasets_access.sql
@@ -1,2 +1,2 @@
 # Update all protocol metadata that are "updated" to dtype = 1
-UPDATE mr_protocol_metadata SET dtype = 2 WHERE id IN (SELECT updated_metadata_id FROM mr_protocol WHERE updated_metadata IS NOT NULL);
+update mr_protocol_metadata set dtype = 2 where mr_protocol_metadata.id in (select distinct updated_metadata_id from mr_protocol where updated_metadata_id is not NULL);
