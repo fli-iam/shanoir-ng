f52e16d2686774fac9d1ab7c3c3ca393c3d25b2c
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2021-07-30 16:35:33 +0200

    Correct preclinical

--- shanoir-ng-preclinical/src/main/java/org/shanoir/ng/preclinical/pathologies/pathology_models/PathologyModelUniqueValidator.java
+++ shanoir-ng-preclinical/src/main/java/org/shanoir/ng/preclinical/pathologies/pathology_models/PathologyModelUniqueValidator.java
@@ -1,7 +1,9 @@
 package org.shanoir.ng.preclinical.pathologies.pathology_models;
 
 import org.shanoir.ng.shared.validation.UniqueConstraintManagerImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PathologyModelUniqueValidator extends UniqueConstraintManagerImpl<PathologyModel> {
 
 }
