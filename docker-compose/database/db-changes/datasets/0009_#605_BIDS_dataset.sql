509fce90654848119625396cfbdba19a7dea4198
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2021-10-28 10:09:12 +0200

    SQL

--- docker-compose/database/db-changes/datasets/0009_#605_BIDS_dataset.sql
+++ docker-compose/database/db-changes/datasets/0009_#605_BIDS_dataset.sql
@@ -3,7 +3,7 @@ DELETE FROM subject_study;
 DELETE FROM subject;
 DELETE FROM tag;
 
-#INSERT INTO subject (id, name) (SELECT id, name FROM studies.subject);
-#INSERT INTO tag (id, name, color, study_id) (SELECT id, name, color, study_id FROM studies.tag);
-#INSERT INTO subject_study (id, subject_id, study_id) (SELECT id, subject_id, study_id FROM studies.subject_study);
-#INSERT INTO subject_study_tag (subject_study_id, tags_id) (SELECT subject_study_id, tags_id FROM studies.subject_study_tag);
+INSERT INTO subject (id, name) (SELECT id, name FROM studies.subject);
+INSERT INTO tag (id, name, color, study_id) (SELECT id, name, color, study_id FROM studies.tag);
+INSERT INTO subject_study (id, subject_id, study_id) (SELECT id, subject_id, study_id FROM studies.subject_study);
+INSERT INTO subject_study_tag (subject_study_id, tags_id) (SELECT subject_study_id, tags_id FROM studies.subject_study_tag);
