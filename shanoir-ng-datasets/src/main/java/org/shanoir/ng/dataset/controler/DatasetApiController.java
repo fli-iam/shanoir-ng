7db5b6617578c0c38e14a2bc841cc5d484c2667e
Alae ES-SAKI <alaeessaki@gmail.com>
2021-10-29 10:49:37 +0100

    changing download returns type and adding action possible values

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/controler/DatasetApiController.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/controler/DatasetApiController.java
@@ -578,20 +578,35 @@ public class DatasetApiController implements DatasetApi, CarminDataApi {
 		}
 	}
 
-	// carmin DATA
-
-	public ResponseEntity<Void> deletePath(@ApiParam(value = "The complete path to delete. It can contain non-encoded slashes.", required=true) @PathVariable("completePath") String completePath) {
-		String accept = request.getHeader("Accept");
-		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+	// CARMIN DATA
+	@Override
+	public ResponseEntity<?> getPath(@ApiParam(value = "the complete path on which to request information. It can contain non-encoded slashes. Except for the \"exists\" action, any request on a non-existing path should return an error", required=true) @PathVariable("completePath") String completePath, @NotNull @ApiParam(value = "The \"content\" action downloads the raw file. If the path points to a directory, a tarball of this directory is returned. The \"exists\" action returns a BooleanResponse object (see definition) indicating if the path exists or not. The \"properties\" action returns a Path object (see definition) with the path properties. The \"list\" action returns a DirectoryList object (see definition) with the properties of all the files of the directory (if the path is not a directory an error must be returned). The \"md5\" action is optional and returns a PathMd5 object (see definition)." ,required=true
+			,allowableValues = "properties, exists, list, md5, content", defaultValue = "content") @Valid @RequestParam(value = "action", required = true, defaultValue = "content") String action, HttpServletResponse response) throws IOException, RestServiceException {
+		// TODO implement those actions
+		switch (action){
+			case "exists":
+			case "list":
+			case "md5":
+			case "properties":
+				return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+			case "content":{
+				downloadDatasetById(Long.parseLong(completePath),null, DCM, response);
+				return new ResponseEntity<Void>(HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 	}
 
-	public void getPath(@ApiParam(value = "the complete path on which to request information. It can contain non-encoded slashes. Except for the \"exists\" action, any request on a non-existing path should return an error", required=true) @PathVariable("completePath") String completePath, @NotNull @ApiParam(value = "The \"content\" action downloads the raw file. If the path points to a directory, a tarball of this directory is returned. The \"exists\" action returns a BooleanResponse object (see definition) indicating if the path exists or not. The \"properties\" action returns a Path object (see definition) with the path properties. The \"list\" action returns a DirectoryList object (see definition) with the properties of all the files of the directory (if the path is not a directory an error must be returned). The \"md5\" action is optional and returns a PathMd5 object (see definition)." ,required=true
-	) @Valid @RequestParam(value = "action", required = true) String action, HttpServletResponse response) throws IOException, RestServiceException {
-		downloadDatasetById(Long.parseLong(completePath),null, DCM, response);
-	}
-
+	@Override
 	public ResponseEntity<org.shanoir.ng.dataset.model.carmin.Path> uploadPath(@ApiParam(value = "The complete path on which to upload data. It can contain non-encoded slashes.", required=true) @PathVariable("completePath") String completePath, @ApiParam(value = "") @Valid @RequestBody UploadData body) {
+		// TODO To implement
 		return new ResponseEntity<org.shanoir.ng.dataset.model.carmin.Path>(HttpStatus.NOT_IMPLEMENTED);
+	}
+
+	@Override
+	public ResponseEntity<Void> deletePath(@ApiParam(value = "The complete path to delete. It can contain non-encoded slashes.", required=true) @PathVariable("completePath") String completePath) {
+		// TODO to implement
+		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
 	}
 
 
