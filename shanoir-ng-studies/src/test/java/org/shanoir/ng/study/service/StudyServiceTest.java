e83a33141622256489478c1dcc21f9853c3e7fc9
JL <jul.louis@gmail.com>
2022-04-15 14:03:03 +0200

    Update StudyServiceTest.java

--- shanoir-ng-studies/src/test/java/org/shanoir/ng/study/service/StudyServiceTest.java
+++ shanoir-ng-studies/src/test/java/org/shanoir/ng/study/service/StudyServiceTest.java
@@ -160,7 +160,7 @@ public class StudyServiceTest {
 	@Test
 	public void saveTest() throws MicroServiceCommunicationException, JsonMappingException, JsonProcessingException {
 		studyService.create(createStudy());
-		Mockito.verify(studyRepository, Mockito.times(2)).save(Mockito.any(Study.class));
+		Mockito.verify(studyRepository, Mockito.times(3)).save(Mockito.any(Study.class));
 	}
 
 	@Test
