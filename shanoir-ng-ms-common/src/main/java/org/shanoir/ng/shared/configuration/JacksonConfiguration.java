4c75f980158a9e8a7ad2a2b6eaab4efc7b2cbabe
Julien Louis <jul.louis@gmail.com>
2022-05-10 13:58:27 +0200

    fix objectmapper config

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/configuration/JacksonConfiguration.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/configuration/JacksonConfiguration.java
@@ -17,6 +17,7 @@ package org.shanoir.ng.shared.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -25,7 +26,9 @@ public class JacksonConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        ObjectMapper mapper = new ObjectMapper()
+        		.registerModule(new JavaTimeModule())
+        		.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
 }
