ca300f310a2b8df34bcb6cb97ccea9446f0e4893
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2022-05-18 14:12:11 +0200

    shanoir-issue#1148 correct SQL

--- docker-compose/database/db-changes/studies/0006_study_user_center.sql
+++ docker-compose/database/db-changes/studies/0006_study_user_center.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `study_user_center` (
   `study_user_id` bigint(20) NOT NULL,
-  `centers_id` bigint(20) NOT NULL,
-  UNIQUE KEY `UK_sd736idk992j4iimveitdi5j3` (`centers_id`),
+  `center_id` bigint(20) NOT NULL,
   KEY `FK8jvoy3dqkninlrimnrb8endp3` (`study_user_id`),
-  CONSTRAINT `FK51yprtlva5a0y0kckgtq2phre` FOREIGN KEY (`centers_id`) REFERENCES `center` (`id`),
+  CONSTRAINT `FK51yprtlva5a0y0kckgtq2phre` FOREIGN KEY (`center_id`) REFERENCES `center` (`id`),
   CONSTRAINT `FK8jvoy3dqkninlrimnrb8endp3` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`)
 )
