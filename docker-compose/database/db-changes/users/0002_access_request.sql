5d00346857bd1ac160ce2a78da364e7b739e3537
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2022-11-30 14:27:06 +0100

    #1073 - access request - SQL init - users

--- docker-compose/database/db-changes/users/0002_access_request.sql
+++ docker-compose/database/db-changes/users/0002_access_request.sql
@@ -4,3 +4,15 @@ ALTER TABLE account_request_info DROP COLUMN service;
 ALTER TABLE account_request_info DROP COLUMN challenge;
 ALTER TABLE account_request_info ADD COLUMN study_id bigint(20);
 ALTER TABLE account_request_info ADD COLUMN study_name varchar(255);
+
+CREATE TABLE `access_request` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `motivation` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `study_id` bigint(20) DEFAULT NULL,
+  `study_name` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKipjw4x52ro7jvnqai3lbqibwh` (`user_id`),
+  CONSTRAINT `FKipjw4x52ro7jvnqai3lbqibwh` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
