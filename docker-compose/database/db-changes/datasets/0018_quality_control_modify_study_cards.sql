641d0fcf991c447cadb77345e7191d39e69d5315
Julien Louis <jul.louis@gmail.com>
2022-12-23 14:33:34 +0100

    fix script

--- docker-compose/database/db-changes/datasets/0018_quality_control_modify_study_cards.sql
+++ docker-compose/database/db-changes/datasets/0018_quality_control_modify_study_cards.sql
@@ -1,5 +1,5 @@
 -- create quality_card
-CREATE TABLE `quality_card` (
+CREATE TABLE quality_card (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   study_id bigint(20) DEFAULT NULL,
@@ -13,8 +13,8 @@ CREATE TABLE quality_examination_rule (
   tag int(11) DEFAULT NULL,
   quality_card_id bigint(20) DEFAULT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT FK8bavfrsgqwil7aei15l00dg6g FOREIGN KEY (quality_card_id) REFERENCES quality_card (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE quality_examination_rule ADD CONSTRAINT FK8bavfrsgqwil7aei15l00dg6g FOREIGN KEY (quality_card_id) REFERENCES quality_card (id);
 
 -- study_card_condition
 ALTER TABLE study_card_condition MODIFY dicom_tag int(11) NULL;
@@ -22,13 +22,10 @@ ALTER TABLE study_card_condition ADD COLUMN shanoir_field int(11) NULL;
 ALTER TABLE study_card_condition ADD COLUMN scope varchar(31) NOT NULL;
 UPDATE study_card_condition SET scope = 'StudyCardDICOMCondition';
 
-SET @dataset_field_ids = (1,6,10,11,14); -- dataset field ids
-SET @acquisition_field_ids = (2,3,4,5,7,8,9,12,13,15); -- acquisition field ids
-
 -- study_card_assignment, add scope and set its value
 ALTER TABLE study_card_assignment ADD COLUMN scope varchar(31) NOT NULL;
-UPDATE study_card_assignment SET scope = 'Dataset' WHERE field in @dataset_field_ids;
-UPDATE study_card_assignment SET scope = 'DatasetAcquisition' WHERE field in @acquisition_field_ids;
+UPDATE study_card_assignment SET scope = 'Dataset' WHERE field IN (1,6,10,11,14);
+UPDATE study_card_assignment SET scope = 'DatasetAcquisition' WHERE field IN (2,3,4,5,7,8,9,12,13,15);
 
 -- study_card_rule, add scope and set its value
 ALTER TABLE study_card_rule ADD COLUMN scope varchar(31) NOT NULL;
