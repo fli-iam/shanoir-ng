25b39f5a7300ace7bcc93b93c2717c6ba3af4630
Anthony Baire <Anthony.Baire@irisa.fr>
2023-01-18 12:53:11 +0100

    use the AS_RECEIVED aet by default in Q/R operations
    This makes dcm4chee return the DICOM objects as they were received
    and stored (it disables attribute coercion and merge of information)

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/download/WADODownloaderService.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/download/WADODownloaderService.java
@@ -54,7 +54,7 @@ import org.springframework.web.client.RestTemplate;
  * WADO-RS URLs are supported: http://dicom.nema.org/DICOM/2013/output/chtml/part18/sect_6.5.html
  * WADO-URI URLs are supported: http://dicom.nema.org/DICOM/2013/output/chtml/part18/sect_6.2.html
  * 
- * WADO-RS: http://dcm4chee-arc:8081/dcm4chee-arc/aets/DCM4CHEE/rs/studies/1.4.9.12.22.1.8447.5189520782175635475761938816300281982444
+ * WADO-RS: http://dcm4chee-arc:8081/dcm4chee-arc/aets/AS_RECEIVED/rs/studies/1.4.9.12.22.1.8447.5189520782175635475761938816300281982444
  * /series/1.4.9.12.22.1.3337.609981376830290333333439326036686033499
  * /instances/1.4.9.12.22.1.3327.13131999371192661094333587030092502791578
  * 
@@ -62,7 +62,7 @@ import org.springframework.web.client.RestTemplate;
  * this class extracts as well the files contained in the response to
  * the file system.
  * 
- * WADO-URI: http://dcm4chee-arc:8081/dcm4chee-arc/aets/DCM4CHEE/wado?requestType=WADO
+ * WADO-URI: http://dcm4chee-arc:8081/dcm4chee-arc/aets/AS_RECEIVED/wado?requestType=WADO
  * &studyUID=1.4.9.12.22.1.8444.518952078217568647576155668816300281982444
  * &seriesUID=1.4.9.12.22.1.8444.60998137683029030014444439326036686033499
  * &objectUID=1.4.9.12.22.1.8444.1313199937119266109555587030092502791578
