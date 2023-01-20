c85aca98664488bfab8e88ef0c3e2b48119bf541
phdauvergne <pierre-henri.dauvergne@inria.fr>
2023-01-20 10:03:35 +0100

    shanoir-issue#1483: annotation on description field

--- shanoir-ng-studies/src/main/java/org/shanoir/ng/study/model/Study.java
+++ shanoir-ng-studies/src/main/java/org/shanoir/ng/study/model/Study.java
@@ -18,20 +18,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ColumnResult;
-import javax.persistence.ConstructorResult;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.PostLoad;
-import javax.persistence.SqlResultSetMapping;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -160,6 +147,8 @@ public class Study extends HalEntity {
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "study", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Tag> tags;
 
+	@Lob
+	@Column(name = "description", columnDefinition = "TEXT")
 	private String description;
 
 	@ElementCollection
