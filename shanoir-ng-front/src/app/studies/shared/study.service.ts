0ed9d938df4f09db2fceeadb9b7b02636d6091d2
Julien Louis <jul.louis@gmail.com>
2023-03-27 10:18:57 +0200

    fix bad merge

--- shanoir-ng-front/src/app/studies/shared/study.service.ts
+++ shanoir-ng-front/src/app/studies/shared/study.service.ts
@@ -27,10 +27,8 @@ import { Profile } from '../../shared/models/profile.model';
 import { SubjectWithSubjectStudy } from '../../subjects/shared/subject.with.subject-study.model';
 import * as AppUtils from '../../utils/app.utils';
 import { StudyUserRight } from './study-user-right.enum';
-import { CenterStudyDTO, PublicStudyDataDTO, StudyDTO, StudyDTOService, SubjectWithSubjectStudyDTO} from './study.dto';
+import { CenterStudyDTO, PublicStudyData, StudyDTO, StudyDTOService, SubjectWithSubjectStudyDTO} from './study.dto';
 import { Study } from './study.model';
-import { Profile } from "../../shared/models/profile.model";
-import { saveAs } from "file-saver";
 
 @Injectable()
 export class StudyService extends EntityService<Study> implements OnDestroy {
