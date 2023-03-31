e151b27736774fff78fbcf26ef22f9349c5b3c68
Arthur Masson <arthur.sw@gmail.com>
2023-03-31 13:59:42 +0200

    rewrite download statistics procedure

--- docker-compose/database/3_add_statistics_procedure.sql
+++ docker-compose/database/3_add_statistics_procedure.sql
@@ -20,71 +20,58 @@ create procedure getStatistics(IN studyNameInRegExp VARCHAR(255), IN studyNameOu
 begin
     select 'patient_id', 'shanoir_name', 'double_hash', 'birthname1', 'birthname2', 'birthname3', 'lastname1', 'lastname2', 'lastname3', 'firstname1', 'firstname2', 'firstname3', 'birthdate1', 'sex', 'birth_year', 'study_id', 'study_name', 'sequence_id', 'norm_sequence_name', 'sequence_name', 'center_id', 'center', 'device_manufacturer', 'device_model', 'device_field_strength', 'device_serial_number', 'examination_id', 'examination_date', 'import_date', 'creation_date', 'protocol_type'
     union all
-    select distinct
-    sb.id as patient_id, 
-    sb.name as shanoir_name, 
-    sb.identifier as double_hash, 
-    pseud.birth_name_hash1 as birthname1,
-    pseud.birth_name_hash2 as birthname2,
-    pseud.birth_name_hash3 as birthname3,
-    pseud.last_name_hash1 as lastname1,
-    pseud.last_name_hash2 as lastname2,
-    pseud.last_name_hash3 as lastname3,
-    pseud.first_name_hash1 as firstname1,
-    pseud.first_name_hash2 as firstname2,
-    pseud.first_name_hash3 as firstname3,
-    pseud.birth_date_hash as birthdate1,
-    (case sb.sex when 1 then 'M' else 'F' end) as sex,
-    year(sb.birth_date) as birth_year,
-    st.id as study_id,
-    st.name as study_name,
-    dt.id as sequence_id,
-    dt_md.name as norm_sequence_name, 
-    dt_md.comment as sequence_name,
-    cnt.id as center_id, 
-    cnt.name as center,
-    man.name as device_manufacturer,
-    man_mod.name as device_model,
-    man_mod.magnetic_field as device_field_strength,
-    ac_eq.serial_number as device_serial_number,
-    ex.id as examination_id,
-    date(ex.examination_date) as examination_date,
-    date(dt_acq.creation_date) as import_date,
-    date(dt.creation_date) as creation_date,
-    pr_md.name as protocol_type
+    select 
+        subject.id as patient_id,
+        subject.name as shanoir_name,
+        subject.identifier as double_hash,
+        pseudonymus_hash_values.birth_name_hash1 as birthname1,
+        pseudonymus_hash_values.birth_name_hash2 as birthname2,
+        pseudonymus_hash_values.birth_name_hash3 as birthname3,
+        pseudonymus_hash_values.last_name_hash1 as lastname1,
+        pseudonymus_hash_values.last_name_hash2 as lastname2,
+        pseudonymus_hash_values.last_name_hash3 as lastname3,
+        pseudonymus_hash_values.first_name_hash1 as firstname1,
+        pseudonymus_hash_values.first_name_hash2 as firstname2,
+        pseudonymus_hash_values.first_name_hash3 as firstname3,
+        pseudonymus_hash_values.birth_date_hash as birthdate1,
+        (case subject.sex when 1 then 'M' else 'F' end) as sex,
+        year(subject.birth_date) as birth_year,
+        study.id as study_id,
+        study.name as study_name,
+        dataset.id as sequence_id,
+        dataset_metadata.name as norm_sequence_name,
+        dataset_metadata.comment as sequence_name,
+        center.id as center_id,
+        center.name as center,
+        manufacturer.name as device_manufacturer,
+        manufacturer_model.name as device_model,
+        manufacturer_model.magnetic_field as device_field_strength,
+        acquisition_equipment.serial_number as device_serial_number,
+        examination.id as examination_id,
+        date(examination.examination_date) as examination_date,
+        date(dataset_acquisition.creation_date) as import_date,
+        date(dataset.creation_date) as creation_date,
+        mr_protocol_metadata.name as protocol_type
 
-    from studies.subject as sb
-    left join studies.pseudonymus_hash_values as pseud on (pseud.id = sb.pseudonymus_hash_values_id)
-    inner join studies.study as st
-    inner join studies.center as cnt
-    inner join studies.manufacturer as man
-    inner join studies.manufacturer_model as man_mod
-    inner join studies.acquisition_equipment as ac_eq
-    inner join datasets.mr_protocol as pr
-    inner join datasets.mr_protocol_metadata as pr_md
-    inner join datasets.examination as ex
-    inner join datasets.dataset as dt
-    inner join datasets.dataset_metadata as dt_md
-    inner join datasets.dataset_acquisition as dt_acq
-    inner join datasets.mr_dataset_acquisition as mr_acq
+    from dataset 
+        left join dataset_acquisition on (dataset_acquisition.id = dataset.dataset_acquisition_id)
+        left join dataset_metadata on (dataset_metadata.id = dataset.updated_metadata_id)
+        left join mr_dataset_acquisition on (mr_dataset_acquisition.id = dataset_acquisition.id)
+        left join mr_protocol on (mr_protocol.id = mr_dataset_acquisition.mr_protocol_id)
+        left join mr_protocol_metadata on (mr_protocol_metadata.id = mr_protocol.id)
+        left join studies.acquisition_equipment as acquisition_equipment on (acquisition_equipment.id = dataset_acquisition.acquisition_equipment_id)
+        left join studies.manufacturer_model as manufacturer_model on (manufacturer_model.id = acquisition_equipment.manufacturer_model_id)
+        left join studies.manufacturer as manufacturer on (manufacturer.id = manufacturer_model.manufacturer_id)
+        left join examination on (examination.id = dataset_acquisition.examination_id)
+        left join studies.subject as subject on (subject.id = dataset.subject_id)
+        left join studies.study as study on (study.id = dataset.study_id)
+        left join studies.center as center on (center.id = examination.center_id)
+        left join studies.pseudonymus_hash_values as pseudonymus_hash_values on (pseudonymus_hash_values.id = subject.pseudonymus_hash_values_id)
 
-    on st.id = ex.study_id
-    and sb.id = ex.subject_id
-    and ex.center_id = cnt.id
-    and sb.id = dt.subject_id
-    and dt.dataset_acquisition_id = dt_acq.id
-    and dt_md.id = dt.updated_metadata_id
-    and dt_acq.acquisition_equipment_id = ac_eq.id
-    and ac_eq.manufacturer_model_id = man_mod.id
-    and man_mod.manufacturer_id = man.id
-    and dt_acq.id = mr_acq.id
-    and mr_acq.mr_protocol_id = pr.id
-    and pr_md.id = pr.id
-    and ex.id = dt_acq.examination_id
-    and sb.name rlike if(subjectNameInRegExp is null or subjectNameInRegExp = '', '.*', subjectNameInRegExp)
-    and sb.name not rlike if(subjectNameOutRegExp is null or subjectNameOutRegExp = '', '^\b\B$', subjectNameOutRegExp)
-    and st.name rlike if(studyNameInRegExp is null or studyNameInRegExp = '', '.*', studyNameInRegExp)
-    and st.name not rlike if(studyNameOutRegExp is null or studyNameOutRegExp = '', '^\b\B$', studyNameOutRegExp);
+    where subject.name rlike if(subjectNameInRegExp is null or subjectNameInRegExp = '', '.*', subjectNameInRegExp)
+        and subject.name not rlike if(subjectNameOutRegExp is null or subjectNameOutRegExp = '', '^\b\B$', subjectNameOutRegExp)
+        and study.name rlike if(studyNameInRegExp is null or studyNameInRegExp = '', '.*', studyNameInRegExp)
+        and study.name not rlike if(studyNameOutRegExp is null or studyNameOutRegExp = '', '^\b\B$', studyNameOutRegExp);
 end //
 
 delimiter ;
