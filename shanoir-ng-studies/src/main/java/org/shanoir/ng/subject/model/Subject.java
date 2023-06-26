ae82d82df710c6d68c26797ca5141e95eb45407f
Youenn Merel <youenn.merel@inria.fr>
2023-06-26 16:58:07 +0200

    [fli-iam#1698] Correct subject entity

--- shanoir-ng-studies/src/main/java/org/shanoir/ng/subject/model/Subject.java
+++ shanoir-ng-studies/src/main/java/org/shanoir/ng/subject/model/Subject.java
@@ -18,18 +18,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.ColumnResult;
-import javax.persistence.ConstructorResult;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.PostLoad;
-import javax.persistence.SqlResultSetMapping;
+import javax.persistence.*;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.GenericGenerator;
 import org.shanoir.ng.shared.core.model.IdName;
 import org.shanoir.ng.shared.dateTime.LocalDateSerializer;
@@ -84,7 +75,8 @@ public class Subject extends HalEntity {
 
 
 	/** Flag to set the subject as pre-clinical subject */
-
+	@Column(nullable=false)
+	@ColumnDefault("false")
 	private boolean preclinical;
 	
 	/**
