763f2f6da1dbfd60f77c9bd1c0922cc446505b7c
Julien Louis <jul.louis@gmail.com>
2023-07-03 13:40:32 +0200

    new solr button : copy selected ids to clipboard

--- shanoir-ng-front/src/app/app.module.ts
+++ shanoir-ng-front/src/app/app.module.ts
@@ -269,7 +269,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginGuard } from "./shared/roles/login-guard";
 import { AccessRequestService } from './users/access-request/access-request.service';
 import { AccessRequestListComponent } from './users/access-request/access-request-list.component';
-
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
     imports: [
@@ -282,7 +282,8 @@ import { AccessRequestListComponent } from './users/access-request/access-reques
         NgxJsonViewerModule,
         AppRoutingModule,
         PreclinicalRoutingModule,
-        RouterModule
+        RouterModule,
+        ClipboardModule
     ],
     declarations: [
         AccountRequestComponent,
