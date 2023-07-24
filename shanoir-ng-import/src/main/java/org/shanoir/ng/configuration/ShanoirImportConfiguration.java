db439b531b75f3630a65c3b1e810b6c3c0121b0f
michaelkain <michael.kain@inria.fr>
2023-07-24 15:20:47 +0200

    Spring security: next issue solved with access denied and wrong role mapping

--- shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/ShanoirImportConfiguration.java
+++ shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/ShanoirImportConfiguration.java
@@ -20,7 +20,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * As Spring Boot >= 1.4 does not automatically provide a RestTemplate
@@ -34,7 +33,6 @@ public class ShanoirImportConfiguration {
 
 	@Bean(name = "asyncExecutor")
     public Executor asyncExecutor() {
-		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(100);
         executor.setMaxPoolSize(100);
--- shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/security/SecurityConfiguration.java
+++ shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/security/SecurityConfiguration.java
@@ -14,6 +14,10 @@
 
 package org.shanoir.ng.configuration.security;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -23,8 +27,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -45,7 +49,7 @@ public class SecurityConfiguration {
 
 	@Value("${front.server.url}")
 	private String frontServerUrl;
-
+	
 	/**
 	 * Defines the session authentication strategy.
 	 */
@@ -55,22 +59,24 @@ public class SecurityConfiguration {
 	}
 
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable()
-				.authorizeHttpRequests()
-				.requestMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
-				.anyRequest().authenticated().and().oauth2ResourceServer().jwt()
-				.jwtAuthenticationConverter(jwtAuthenticationConverter());
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+			.csrf().disable()
+		.authorizeHttpRequests()
+			.requestMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+			.anyRequest().authenticated()
+		.and()
+			.oauth2ResourceServer(oauth2Configurer -> oauth2Configurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+                Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+                Collection<String> roles = realmAccess.get("roles");
+                var grantedAuthorities = roles.stream()
+                        .map(role -> new SimpleGrantedAuthority(role))
+                        .collect(Collectors.toList());
+                return new JwtAuthenticationToken(jwt, grantedAuthorities);
+            })));
 		return http.build();
-	}
-
-	private JwtAuthenticationConverter jwtAuthenticationConverter() {
-		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-		jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-		return jwtAuthenticationConverter;
 	}
 
 	@Bean
