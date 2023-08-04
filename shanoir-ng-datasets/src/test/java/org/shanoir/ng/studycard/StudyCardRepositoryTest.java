4f808650934cfef60b574c481dd8fca9316814bb
michaelkain <michael.kain@inria.fr>
2023-08-04 14:49:11 +0200

    Update StudyCardRepositoryTest.java

--- shanoir-ng-datasets/src/test/java/org/shanoir/ng/studycard/StudyCardRepositoryTest.java
+++ shanoir-ng-datasets/src/test/java/org/shanoir/ng/studycard/StudyCardRepositoryTest.java
@@ -39,7 +39,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StudyCardRepositoryTest {
 
 	private static final String STUDYCARD_TEST_1_DATA = "StudyCard1";
