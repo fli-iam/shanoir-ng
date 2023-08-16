62be9c36ec44b196a7c0d09c804d330b6f5a1d5b
Youenn Merel <youenn.merel@inria.fr>
2023-08-16 16:58:24 +0200

    Revert "[fli-iam#1280] Correct groupBy init"
    This reverts commit 4d25cf715476cf1d55309a66ddb179b3e5485ad1.

--- shanoir-ng-front/src/assets/env.js
+++ shanoir-ng-front/src/assets/env.js
@@ -2,6 +2,5 @@
   window["env"] = window["env"] || {};
   // Environment variables
   window["env"]["viewerUrl"] = "SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST";
-  // window["env"]["vipUrl"] = "VIP_URL_SCHEME://VIP_URL_HOST";
-    window["env"]["vipUrl"] = "http://shanoir-ng-nginx:3001";
+  window["env"]["vipUrl"] = "VIP_URL_SCHEME://VIP_URL_HOST";
 })(this);
