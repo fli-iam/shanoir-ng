4d25cf715476cf1d55309a66ddb179b3e5485ad1
Youenn Merel <youenn.merel@inria.fr>
2023-08-16 16:57:37 +0200

    [fli-iam#1280] Correct groupBy init

--- shanoir-ng-front/src/assets/env.js
+++ shanoir-ng-front/src/assets/env.js
@@ -2,5 +2,6 @@
   window["env"] = window["env"] || {};
   // Environment variables
   window["env"]["viewerUrl"] = "SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST";
-  window["env"]["vipUrl"] = "VIP_URL_SCHEME://VIP_URL_HOST";
+  // window["env"]["vipUrl"] = "VIP_URL_SCHEME://VIP_URL_HOST";
+    window["env"]["vipUrl"] = "http://shanoir-ng-nginx:3001";
 })(this);
