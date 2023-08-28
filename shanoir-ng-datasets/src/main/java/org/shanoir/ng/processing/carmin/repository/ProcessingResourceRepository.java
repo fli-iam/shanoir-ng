77897f38c132253f19062f31d383872814d2f78d
Youenn Merel <youenn.merel@inria.fr>
2023-08-28 13:16:34 +0200

    [fli-iam#1280] Correct processing resource repository

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/repository/ProcessingResourceRepository.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/repository/ProcessingResourceRepository.java
@@ -2,6 +2,7 @@ package org.shanoir.ng.processing.carmin.repository;
 
 import org.shanoir.ng.dataset.model.Dataset;
 import org.shanoir.ng.processing.carmin.model.ProcessingResource;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface ProcessingResourceRepository  extends CrudRepository<ProcessingResource, Long> {
 
+    @Query(value = "SELECT dataset.id FROM ProcessingResource WHERE resourceId = :resourceId")
     List<Long> findDatasetIdByResourceId(String resourceId);
 }
