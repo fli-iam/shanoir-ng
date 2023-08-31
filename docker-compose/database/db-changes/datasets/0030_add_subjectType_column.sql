fe9b8ba3a4b4a9d51b3454ed61930b197ad4380c
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2023-08-31 16:58:22 +0200

    shanoir-issue#1219: new db-change script

--- docker-compose/database/db-changes/datasets/0030_add_subjectType_column.sql
+++ docker-compose/database/db-changes/datasets/0030_add_subjectType_column.sql
@@ -1,5 +1,1 @@
 ALTER TABLE subject_study ADD COLUMN subject_type int(11) DEFAULT NULL;
-
-UPDATE datasets.subject_study JOIN studies.subject_study ON datasets.subject_study.id = studies.subject_study.id SET datasets.subject_study.subject_type = studies.subject_study.subject_type;
-
-INSERT INTO datasets.acquisition_equipment (id, name) SELECT smm.id AS id, CONCAT(sm.name, ' ', smm.name) AS name FROM studies.manufacturer_model AS smm INNER JOIN studies.manufacturer AS sm ON smm.manufacturer_id = sm.id;
