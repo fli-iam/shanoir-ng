c89423a1aa2adf24740982d23249c35f714c6e2b
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2023-09-04 13:56:44 +0200

    shanoir-issue#1219: add db-change script that add column acquisition_equipment_name in shanoir_metadata

--- docker-compose/database/db-changes/datasets/0032_populate_subjectType_and_acEq.sql
+++ docker-compose/database/db-changes/datasets/0032_populate_subjectType_and_acEq.sql
@@ -1,3 +1,5 @@
+ALTER TABLE shanoir_metadata ADD COLUMN acquisition_equipment_name varchar(255);
+
 UPDATE datasets.subject_study JOIN studies.subject_study ON datasets.subject_study.id = studies.subject_study.id SET datasets.subject_study.subject_type = studies.subject_study.subject_type;
 
 INSERT INTO datasets.acquisition_equipment (id, name) SELECT smm.id AS id, CONCAT(sm.name, ' ', smm.name) AS name FROM studies.manufacturer_model AS smm INNER JOIN studies.manufacturer AS sm ON smm.manufacturer_id = sm.id;
