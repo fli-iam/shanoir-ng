d82f058e10b14ca57f4552c9978351c06e055a78
jcomedouteau <7604176+jcomedouteau@users.noreply.github.com>
2023-09-12 11:01:18 +0200

    [fli-iam#1478] - BIDS tree specs compliance
    Subject/study/exam folder name compliance

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/bids/controller/BidsApiController.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/bids/controller/BidsApiController.java
@@ -70,6 +70,13 @@ public class BidsApiController implements BidsApi {
 		return ResponseEntity.ok().build();
 	}
 
+    public ResponseEntity<BidsElement> refreshBIDSByStudyId(
+    		@Parameter(name = "id of the study", required=true) @PathVariable("studyId") Long studyId,
+    		@Parameter(name = "name of the study", required=true) @PathVariable("studyName") String studyName) throws RestServiceException, IOException {
+    	this.bidsService.deleteBidsFolder(studyId, studyName);
+    	return this.getBIDSStructureByStudyId(studyId);
+    }
+
 	@Override
     public void exportBIDSFile(
     		@Parameter(name = "Id of the study", required=true) @PathVariable("studyId") Long studyId,
--- shanoir-ng-datasets/src/test/java/org/shanoir/ng/exporter/service/BidsServiceTest.java
+++ shanoir-ng-datasets/src/test/java/org/shanoir/ng/exporter/service/BidsServiceTest.java
@@ -136,7 +136,7 @@ public class BidsServiceTest {
 		File studyFile = new File(tempFolderPath + "stud-" + exam.getStudyId() + "_" + studyName);
 		assertTrue(studyFile.exists());
 
-		File subjectFile = new File(studyFile.getAbsolutePath() + "/sub-1_" + subject.getName());
+		File subjectFile = new File(studyFile.getAbsolutePath() + "/sub-1-" + subject.getName());
 		assertTrue(subjectFile.exists());
 
 		File examFile = new File(subjectFile.getAbsolutePath() + "/ses-" + exam.getId());
