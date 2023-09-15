3fc3bc54256dd17cb312d9b635d7f57f42a23504
Youenn Merel <youenn.merel@inria.fr>
2023-09-15 11:24:45 +0200

    Log file paths

--- shanoir-ng-preclinical/src/main/java/org/shanoir/ng/preclinical/extra_data/ExtraDataApiController.java
+++ shanoir-ng-preclinical/src/main/java/org/shanoir/ng/preclinical/extra_data/ExtraDataApiController.java
@@ -357,11 +357,16 @@ public class ExtraDataApiController implements ExtraDataApi {
 	private ExaminationExtraData saveUploadedFile(ExaminationExtraData extradata, MultipartFile file) throws IOException {
 		
 		File createdFolder = new File(extraDataPath + "/models/" + extradata.getId());
-		createdFolder.mkdirs();
+		if(!createdFolder.mkdirs()){
+			LOG.error("Could not create directory [{}]", createdFolder.getAbsolutePath());
+		}
+
 		// Path to file
 		File fileToGet = new File(createdFolder + "/" + file.getOriginalFilename());
 		try {
-			fileToGet.createNewFile();
+			if(!fileToGet.createNewFile()){
+				LOG.error("Could not create file [{}]", fileToGet.getAbsolutePath());
+			}
 			file.transferTo(fileToGet);
 		} catch (IOException e) {
 			LOG.error("Error while saving file [{}] to [{}].", file.getOriginalFilename(), fileToGet.getAbsolutePath());
