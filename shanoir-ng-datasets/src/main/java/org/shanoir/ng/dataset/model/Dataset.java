4de1b457ed30280fa39402c98ce46c46be280233
Youenn Merel <youenn.merel@inria.fr>
2023-09-19 17:11:31 +0200

    [fli-iam#1864] Remove NIRS dataset type

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/model/Dataset.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/model/Dataset.java
@@ -83,7 +83,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 		@JsonSubTypes.Type(value = StatisticalDataset.class, name = "Statistical"),
 		@JsonSubTypes.Type(value = TemplateDataset.class, name = "Template"),
 		@JsonSubTypes.Type(value = BidsDataset.class, name = "BIDS"),
-		@JsonSubTypes.Type(value = BidsDataset.class, name = "NIRS"),
 		@JsonSubTypes.Type(value = MeasurementDataset.class, name = "Measurement") })
 public abstract class Dataset extends AbstractEntity {
 
