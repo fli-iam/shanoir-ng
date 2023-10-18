1553b65b5ab02ca9c00b5c36777eab8ff0b57218
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2023-10-18 11:15:13 +0200

    shanoir-issue#1905-2: regroup two if into one

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/dto/ImportJob.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/dto/ImportJob.java
@@ -225,15 +225,11 @@ public class ImportJob implements Serializable {
     }
 
     public Serie getFirstSerie() {
-        if ( getPatients() == null
+        if ( getPatients() == null || getPatients().size() == 0
                 || getPatients().get(0) == null
-                || getPatients().get(0).getStudies() == null
+                || getPatients().get(0).getStudies() == null || getPatients().get(0).getStudies().size() == 0
                 || getPatients().get(0).getStudies().get(0) == null
-                || getPatients().get(0).getStudies().get(0).getSeries() == null) {
-            return null;
-        } else if (getPatients().size() == 0
-                || getPatients().get(0).getStudies().size() == 0
-                || getPatients().get(0).getStudies().get(0).getSeries().size() == 0) {
+                || getPatients().get(0).getStudies().get(0).getSeries() == null || getPatients().get(0).getStudies().get(0).getSeries().size() == 0) {
             return null;
         } else {
             return getPatients().get(0).getStudies().get(0).getSeries().get(0);
