19a00787209f03a616cee6d295daf44ea9406c58
Julien Louis <jul.louis@gmail.com>
2023-12-06 14:24:00 +0100

    remove logs

--- shanoir-ng-front/src/app/shared/notifications/notifications.service.ts
+++ shanoir-ng-front/src/app/shared/notifications/notifications.service.ts
@@ -161,7 +161,6 @@ export class NotificationsService {
             this.source.addEventListener('message', message => {
                 if (message.data !== "{}") {
                     let task: Task = this.taskService.toRealObject(JSON.parse(message.data));
-                    console.log(task);
                     let existingTask = this.tasks.find(t => t.completeId == task.completeId);
                     if (existingTask) {
                         existingTask.updateWith(task);
