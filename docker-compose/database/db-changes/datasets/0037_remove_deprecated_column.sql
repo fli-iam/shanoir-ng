c948341a3dade1e273123ffcf427541c277993df
Julien Louis <jul.louis@gmail.com>
2024-01-17 17:04:41 +0100

    revert + squash wo name change

--- docker-compose/database/db-changes/datasets/0037_remove_deprecated_column.sql
+++ docker-compose/database/db-changes/datasets/0037_remove_deprecated_column.sql
@@ -1,3 +1,9 @@
+-- the rule_id column has been replaced by a join table and all associations has already been copied in it
+-- see docker-compose/database/db-changes/datasets/0023_quality_control_modify_study_cards.sql
+alter table study_card_condition drop foreign key FKpi8v33fmd0wn64e06q8it8pkv;
+alter table study_card_condition drop column rule_id;
+
+-- squashed
 DELETE from datasets.acquisition_equipment;
 
 INSERT INTO datasets.acquisition_equipment (id, name)
