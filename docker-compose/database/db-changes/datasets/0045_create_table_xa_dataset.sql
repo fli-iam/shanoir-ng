39e7b07af68ab4ffe5e40584ea68eabbafdaf081
LaurentPV <laurentpaulvallet@gmail.com>
2024-02-14 15:09:58 +0100

    correction of xa dataset sql script

--- docker-compose/database/db-changes/datasets/0045_create_table_xa_dataset.sql
+++ docker-compose/database/db-changes/datasets/0045_create_table_xa_dataset.sql
@@ -2,7 +2,12 @@ CREATE TABLE `xa_dataset` (
   `id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `FKg6kp2mt6t11qtjg80j57agkn6` FOREIGN KEY (`id`) REFERENCES `dataset` (`id`)
-)
+);
+
+CREATE TABLE `xa_protocol` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `xa_dataset_acquisition` (
   `id` bigint(20) NOT NULL,
@@ -11,9 +16,4 @@ CREATE TABLE `xa_dataset_acquisition` (
   UNIQUE KEY `UK_7ewg4h5ec3h4mjer364hunbk9` (`xa_protocol_id`),
   CONSTRAINT `FK5sv6h57xmxil97cbsk8dwsw1j` FOREIGN KEY (`xa_protocol_id`) REFERENCES `xa_protocol` (`id`),
   CONSTRAINT `FK9n4ys7m737wmmv5l3u4qslcdx` FOREIGN KEY (`id`) REFERENCES `dataset_acquisition` (`id`)
-)
-
-CREATE TABLE `xa_protocol` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-)
+);
