3ddd565a3c529ba8faca4affbb5e4dffce59d5a0
michaelkain <michael.kain@inria.fr>
2024-02-22 15:21:35 +0100

    Update ShUpConfig.java

--- shanoir-uploader/src/main/java/org/shanoir/uploader/ShUpConfig.java
+++ shanoir-uploader/src/main/java/org/shanoir/uploader/ShUpConfig.java
@@ -20,7 +20,7 @@ public class ShUpConfig {
 	 */
 	public static final String SHANOIR_UPLOADER_VERSION = "v8.0.0";
 	
-	public static final String RELEASE_DATE = "2024-02-20 16:32";
+	public static final String RELEASE_DATE = "2024-02-22 15:21";
 	
 	public static final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 	
