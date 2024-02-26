6f10344accb735935e56edf81097b4dbebc238d0
michaelkain <michael.kain@inria.fr>
2024-02-26 16:09:41 +0100

    Update LatestCheckedExamination.java

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/schedule/LatestCheckedExamination.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/schedule/LatestCheckedExamination.java
@@ -1,12 +1,15 @@
 package org.shanoir.ng.examination.schedule;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class LatestCheckedExamination {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long examinationId;
