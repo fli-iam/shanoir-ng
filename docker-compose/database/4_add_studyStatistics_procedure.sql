10cd13f5e6cd2c66ca14406be76e708893d49f16
LaurentPV <laurentpaulvallet@gmail.com>
2024-03-01 17:03:56 +0100

    modifying where condition of query

--- docker-compose/database/4_add_studyStatistics_procedure.sql
+++ docker-compose/database/4_add_studyStatistics_procedure.sql
@@ -58,7 +58,7 @@ INNER JOIN
     studies.subject s ON ss.subject_id = s.id
 INNER JOIN
     studies.study_center sc ON c.id = sc.center_id AND e.study_id = sc.study_id
-WHERE e.study_id = studyId;
+WHERE e.study_id = studyId AND ss.study_id = studyId AND sc.study_id = studyId;
 END //
 
 delimiter ;
--- docker-compose/database/db-changes/studies/0018_add_studyStatistics_procedure.sql
+++ docker-compose/database/db-changes/studies/0018_add_studyStatistics_procedure.sql
@@ -54,12 +54,12 @@ INNER JOIN
 INNER JOIN
     datasets.dataset_metadata dm ON d.updated_metadata_id = dm.id
 INNER JOIN
-    studies.subject_study ss ON d.subject_id = ss.subject_id AND d.study_id = ss.study_id
+    studies.subject_study ss ON d.subject_id = ss.subject_id
 INNER JOIN 
     studies.subject s ON ss.subject_id = s.id
 INNER JOIN
-    studies.study_center sc ON c.id = sc.center_id AND e.study_id = sc.study_id
-WHERE e.study_id = studyId;
+    studies.study_center sc ON c.id = sc.center_id
+WHERE e.study_id = studyId AND ss.study_id = studyId AND sc.study_id = studyId;
 END //
 
 delimiter ;
