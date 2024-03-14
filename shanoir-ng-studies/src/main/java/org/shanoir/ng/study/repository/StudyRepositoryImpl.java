9cbfe13e9877163b837771e2370406440b5f4b6a
LaurentPV <laurentpaulvallet@gmail.com>
2024-03-14 15:57:26 +0100

    Add columns exam comment and dataset name

--- shanoir-ng-studies/src/main/java/org/shanoir/ng/study/repository/StudyRepositoryImpl.java
+++ shanoir-ng-studies/src/main/java/org/shanoir/ng/study/repository/StudyRepositoryImpl.java
@@ -43,35 +43,23 @@ public class StudyRepositoryImpl implements StudyRepositoryCustom {
 
 		for (Object[] row : results) {
 
-			LOG.error("Index : 0 - value {}", row[0]);
-			LOG.error("Index : 1 - value {}", row[1]);
-			LOG.error("Index : 2 - value {}", row[2]);
-			LOG.error("Index : 3 - value {}", row[3]);
-			LOG.error("Index : 4 - value {}", row[4]);
-			LOG.error("Index : 5 - value {}", row[5]);
-			LOG.error("Index : 6 - value {}", row[6]);
-			LOG.error("Index : 7 - value {}", row[7]);
-			LOG.error("Index : 8 - value {}", row[8]);
-			LOG.error("Index : 9 - value {}", row[9]);
-			LOG.error("Index : 10 - value {}", row[10]);
-			LOG.error("Index : 11 - value {}", row[11]);
-			LOG.error("Index : 12 - value {}", row[12]);
-
 			StudyStatisticsDTO dto = new StudyStatisticsDTO();
 
-			dto.setStudyId(Long.parseLong(row[0].toString()));
-			dto.setCenterId(Long.parseLong(row[1].toString()));
-			dto.setCenterName(String.valueOf(row[2]));
-			dto.setCenterPrefix(String.valueOf(row[3]));
-			dto.setSubjectId(Long.parseLong(row[4].toString()));
-			dto.setCommonName(String.valueOf(row[5]));
-			dto.setExaminationId(Long.parseLong(row[6].toString()));
-			dto.setExaminationDate(Date.valueOf(row[7].toString()));
-			dto.setDatasetAcquisitionId(Long.parseLong(row[8].toString()));
-			dto.setImportDate(Date.valueOf(row[9].toString()));
-			dto.setDatasetId(Long.parseLong(row[10].toString()));
-			dto.setModality(String.valueOf(row[11]));
-			dto.setQuality(String.valueOf(row[12]));
+			dto.setStudyId((Long) row[0]);
+			dto.setCenterId((Long) row[1]);
+			dto.setCenterName((String) row[2]);
+			dto.setCenterPrefix((String) row[3]);
+			dto.setSubjectId((Long) row[4]);
+			dto.setCommonName((String) row[5]);
+			dto.setExaminationId((Long) row[6]);
+			dto.setExaminationComment((String) row[7]);
+			dto.setExaminationDate((Date) row[8]);
+			dto.setDatasetAcquisitionId((Long) row[9]);
+			dto.setImportDate((Date) row[10]);
+			dto.setDatasetId((Long) row[11]);
+			dto.setDatasetName((String) row[12]);
+			dto.setModality((String) row[13]);
+			dto.setQuality((String) row[14]);
 
 			studyStatisticsList.add(dto);
 
