d6623ea733e24a29de72fdb970b223996a52dae8
Julien Louis <jul.louis@gmail.com>
2024-03-14 15:02:29 +0100

    log level from warn to info

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/configuration/amqp/RabbitMQDatasetsService.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/configuration/amqp/RabbitMQDatasetsService.java
@@ -482,15 +482,15 @@ public class RabbitMQDatasetsService {
 				event.setProgress(progress);
 				eventService.publishEvent(event);
 
-				LOG.warn("[CopyDatasets] Start copy for dataset " + datasetParentId + " to study " + studyId);
+				LOG.info("[CopyDatasets] Start copy for dataset " + datasetParentId + " to study " + studyId);
 				Long dsCount = datasetRepository.countDatasetsBySourceIdAndStudyId(datasetParentId, studyId);
 				Dataset datasetParent = datasetService.findById(datasetParentId);
 
 				if (datasetParent.getSourceId() != null) {
-					LOG.warn("[CopyDatasets] Selected dataset is a copy, please pick the original dataset.");
+					LOG.info("[CopyDatasets] Selected dataset is a copy, please pick the original dataset.");
 					countCopy++;
 				} else if (dsCount != 0) {
-					LOG.warn("[CopyDatasets] Dataset already exists in this study, copy aborted.");
+					LOG.info("[CopyDatasets] Dataset already exists in this study, copy aborted.");
 					countAlreadyExist++;
 
 				} else {
@@ -498,7 +498,7 @@ public class RabbitMQDatasetsService {
 					Long newDsId = (Long) result[0];
 					countProcessed += (int) result[1];
 					countSuccess += (int) result[2];
-					LOG.warn("countProcessed : " + countProcessed);
+					LOG.info("countProcessed : " + countProcessed);
 					if (newDsId != null)
 						newDatasets.add(newDsId);
 				}
