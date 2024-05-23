02907193c5b618c6b9dc4c48fc88e910e8bbb7cd
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-05-23 11:20:50 +0200

    shanoir-issue#2222: test remove extensions

--- docker-compose/nginx/viewer/app-config.js
+++ docker-compose/nginx/viewer/app-config.js
@@ -1,7 +1,7 @@
 window.config = {
   // default: '/'
   routerBasename: '/',
-  extensions: ['cornerstone', 'dicom-p10-downloader'],
+  extensions: [],
   showStudyList: true,
   filterQueryParam: false,
   disableServersCache: false,
