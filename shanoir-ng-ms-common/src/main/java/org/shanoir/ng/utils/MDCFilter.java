4113cb698cd3421e52da742d56d60e7b7723faaf
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2024-06-19 16:35:33 +0200

    #2141-log-unification
    Unify log pattern over the app

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/utils/MDCFilter.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/utils/MDCFilter.java
@@ -1,10 +1,15 @@
 package org.shanoir.ng.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +25,20 @@ import java.io.IOException;
 @Component
 public class MDCFilter implements Filter {
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private static final Logger LOG = LoggerFactory.getLogger(MDCFilter.class);
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated() && authentication instanceof JwtAuthenticationToken) {
-            if (authentication.getPrincipal() instanceof User principal) {
-                MDC.put("username", principal.getUsername());
-            } else {
-                MDC.put("username", "Admin");
-            }
+            JwtAuthenticationToken jwtauthentication = (JwtAuthenticationToken) authentication;
+            String userId = "" + jwtauthentication.getToken().getClaim("userId");
+            String userName = "" + jwtauthentication.getToken().getClaim("preferred_username");
+            MDC.put("username", userName + "(" + userId + ")");
         } else {
             MDC.put("username", "Admin");
         }
