881fd68ded1e606b86246720240a602724cfafca
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-06-19 16:12:51 +0200

    shanoir-issue#2222: try from another app-config.js source

--- docker-compose/nginx/viewer/app-config.js
+++ docker-compose/nginx/viewer/app-config.js
@@ -1,153 +1,114 @@
 window.config = {
-  routerBasename: '/',
-  // whiteLabeling: {},
+  routerBasename: "/ohif",
   extensions: [],
   modes: [],
-  customizationService: {
-    dicomUploadComponent:
-      '@ohif/extension-cornerstone.customizationModule.cornerstoneDicomUploadComponent',
-  },
+  customizationService: {},
   showStudyList: true,
-  // some windows systems have issues with more than 3 web workers
   maxNumberOfWebWorkers: 3,
-  // below flag is for performance reasons, but it might not work for all servers
+  omitQuotationForMultipartRequest: true,
   showWarningMessageForCrossOrigin: true,
   showCPUFallbackMessage: true,
   showLoadingIndicator: true,
   strictZSpacingForVolumeViewport: true,
-  groupEnabledModesFirst: true,
-  // filterQueryParam: false,
-  defaultDataSourceName: 'dicomweb',
-  /* Dynamic config allows user to pass "configUrl" query string this allows to load config without recompiling application. The regex will ensure valid configuration source */
-  // dangerouslyUseDynamicConfig: {
-  //   enabled: true,
-  //   // regex will ensure valid configuration source and default is /.*/ which matches any character. To use this, setup your own regex to choose a specific source of configuration only.
-  //   // Example 1, to allow numbers and letters in an absolute or sub-path only.
-  //   // regex: /(0-9A-Za-z.]+)(\/[0-9A-Za-z.]+)*/
-  //   // Example 2, to restricts to either hosptial.com or othersite.com.
-  //   // regex: /(https:\/\/hospital.com(\/[0-9A-Za-z.]+)*)|(https:\/\/othersite.com(\/[0-9A-Za-z.]+)*)/
-  //   regex: /.*/,
-  // },
-  dataSources: [
+  maxNumRequests: { interaction: 100, thumbnail: 75, prefetch: 25 },
+  defaultDataSourceName: "dicomweb",
+  oidc: [
     {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'dicomweb',
+      authority: 'SHANOIR_URL_SCHEME://SHANOIR_URL_HOST/auth/realms/shanoir-ng',
+      client_id: 'ohif-viewer',
+      redirect_uri: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/callback',
+      response_type: 'code',
+      scope: 'openid',
+      post_logout_redirect_uri: 'SHANOIR_URL_SCHEME://SHANOIR_URL_HOST'
+    }
+  ],dataSources: [
+    {
+      friendlyName: "dcmjs DICOMWeb Server",
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomweb",
+      sourceName: "dicomweb",
       configuration: {
-        friendlyName: 'DICOMWeb',
         name: 'SHANOIR-NG',
         wadoUriRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/shanoir-ng/',
         qidoRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/dicomweb',
         wadoRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/dicomweb',
-        qidoSupportsIncludeField: true,
-        supportsReject: true,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
+        qidoSupportsIncludeField: false,
+        supportsReject: false,
+        imageRendering: "wadors",
+        thumbnailRendering: "wadors",
         enableStudyLazyLoad: true,
-        useBulkDataURI: false,
-        supportsFuzzyMatching: true,
+        supportsFuzzyMatching: false,
         supportsWildcard: true,
-        dicomUploadEnabled: true,
+        staticWado: true,
+        singlepart: "bulkdata,video",
+        bulkDataURI: { enabled: true, relativeResolution: "studies" },
       },
     },
+    {
+      friendlyName: "dicomweb delegating proxy",
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomwebproxy",
+      sourceName: "dicomwebproxy",
+      configuration: { name: "dicomwebproxy" },
+    },
+    {
+      friendlyName: "dicom json",
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomjson",
+      sourceName: "dicomjson",
+      configuration: { name: "json" },
+    },
+    {
+      friendlyName: "dicom local",
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomlocal",
+      sourceName: "dicomlocal",
+      configuration: {},
+    },
   ],
-  httpErrorHandler: error => {
-    // This is 429 when rejected from the public idc sandbox too often.
-    console.warn(error.status);
-
-    // Could use services manager here to bring up a dialog/modal if needed.
-    console.warn('test, navigate to https://ohif.org/');
+  httpErrorHandler: (e) => {
+    console.warn(e.status), console.warn("test, navigate to https://ohif.org/")
   },
   hotkeys: [
     {
-      commandName: 'incrementActiveViewport',
-      label: 'Next Viewport',
-      keys: ['right'],
+      commandName: "incrementActiveViewport",
+      label: "Next Viewport",
+      keys: ["right"],
     },
     {
-      commandName: 'decrementActiveViewport',
-      label: 'Previous Viewport',
-      keys: ['left'],
+      commandName: "decrementActiveViewport",
+      label: "Previous Viewport",
+      keys: ["left"],
     },
-    { commandName: 'rotateViewportCW', label: 'Rotate Right', keys: ['r'] },
-    { commandName: 'rotateViewportCCW', label: 'Rotate Left', keys: ['l'] },
-    { commandName: 'invertViewport', label: 'Invert', keys: ['i'] },
+    { commandName: "rotateViewportCW", label: "Rotate Right", keys: ["r"] },
+    { commandName: "rotateViewportCCW", label: "Rotate Left", keys: ["l"] },
+    { commandName: "invertViewport", label: "Invert", keys: ["i"] },
     {
-      commandName: 'flipViewportHorizontal',
-      label: 'Flip Horizontally',
-      keys: ['h'],
-    },
-    {
-      commandName: 'flipViewportVertical',
-      label: 'Flip Vertically',
-      keys: ['v'],
-    },
-    { commandName: 'scaleUpViewport', label: 'Zoom In', keys: ['+'] },
-    { commandName: 'scaleDownViewport', label: 'Zoom Out', keys: ['-'] },
-    { commandName: 'fitViewportToWindow', label: 'Zoom to Fit', keys: ['='] },
-    { commandName: 'resetViewport', label: 'Reset', keys: ['space'] },
-    { commandName: 'nextImage', label: 'Next Image', keys: ['down'] },
-    { commandName: 'previousImage', label: 'Previous Image', keys: ['up'] },
-    // {
-    //   commandName: 'previousViewportDisplaySet',
-    //   label: 'Previous Series',
-    //   keys: ['pagedown'],
-    // },
-    // {
-    //   commandName: 'nextViewportDisplaySet',
-    //   label: 'Next Series',
-    //   keys: ['pageup'],
-    // },
-    {
-      commandName: 'setToolActive',
-      commandOptions: { toolName: 'Zoom' },
-      label: 'Zoom',
-      keys: ['z'],
-    },
-    // ~ Window level presets
-    {
-      commandName: 'windowLevelPreset1',
-      label: 'W/L Preset 1',
-      keys: ['1'],
+      commandName: "flipViewportHorizontal",
+      label: "Flip Horizontally",
+      keys: ["h"],
     },
     {
-      commandName: 'windowLevelPreset2',
-      label: 'W/L Preset 2',
-      keys: ['2'],
+      commandName: "flipViewportVertical",
+      label: "Flip Vertically",
+      keys: ["v"],
     },
+    { commandName: "scaleUpViewport", label: "Zoom In", keys: ["+"] },
+    { commandName: "scaleDownViewport", label: "Zoom Out", keys: ["-"] },
+    { commandName: "fitViewportToWindow", label: "Zoom to Fit", keys: ["="] },
+    { commandName: "resetViewport", label: "Reset", keys: ["space"] },
+    { commandName: "nextImage", label: "Next Image", keys: ["down"] },
+    { commandName: "previousImage", label: "Previous Image", keys: ["up"] },
     {
-      commandName: 'windowLevelPreset3',
-      label: 'W/L Preset 3',
-      keys: ['3'],
+      commandName: "setToolActive",
+      commandOptions: { toolName: "Zoom" },
+      label: "Zoom",
+      keys: ["z"],
     },
-    {
-      commandName: 'windowLevelPreset4',
-      label: 'W/L Preset 4',
-      keys: ['4'],
-    },
-    {
-      commandName: 'windowLevelPreset5',
-      label: 'W/L Preset 5',
-      keys: ['5'],
-    },
-    {
-      commandName: 'windowLevelPreset6',
-      label: 'W/L Preset 6',
-      keys: ['6'],
-    },
-    {
-      commandName: 'windowLevelPreset7',
-      label: 'W/L Preset 7',
-      keys: ['7'],
-    },
-    {
-      commandName: 'windowLevelPreset8',
-      label: 'W/L Preset 8',
-      keys: ['8'],
-    },
-    {
-      commandName: 'windowLevelPreset9',
-      label: 'W/L Preset 9',
-      keys: ['9'],
-    },
+    { commandName: "windowLevelPreset1", label: "W/L Preset 1", keys: ["1"] },
+    { commandName: "windowLevelPreset2", label: "W/L Preset 2", keys: ["2"] },
+    { commandName: "windowLevelPreset3", label: "W/L Preset 3", keys: ["3"] },
+    { commandName: "windowLevelPreset4", label: "W/L Preset 4", keys: ["4"] },
+    { commandName: "windowLevelPreset5", label: "W/L Preset 5", keys: ["5"] },
+    { commandName: "windowLevelPreset6", label: "W/L Preset 6", keys: ["6"] },
+    { commandName: "windowLevelPreset7", label: "W/L Preset 7", keys: ["7"] },
+    { commandName: "windowLevelPreset8", label: "W/L Preset 8", keys: ["8"] },
+    { commandName: "windowLevelPreset9", label: "W/L Preset 9", keys: ["9"] },
   ],
-};
+}
