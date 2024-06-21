3e4403e67e517ae45422d5086028bbd73909f01e
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-06-21 11:39:47 +0200

    shanoir-issue#2222: to make keycloak work, parameter needs to be at false

--- docker-compose/nginx/viewer/app-config.js
+++ docker-compose/nginx/viewer/app-config.js
@@ -20,7 +20,7 @@ window.config = {
         wadoUriRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/shanoir-ng/',
         qidoRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/dicomweb',
         wadoRoot: 'SHANOIR_VIEWER_OHIF_URL_SCHEME://SHANOIR_VIEWER_OHIF_URL_HOST/dicomweb',
-        qidoSupportsIncludeField: true,
+        qidoSupportsIncludeField: false,
         supportsReject: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
