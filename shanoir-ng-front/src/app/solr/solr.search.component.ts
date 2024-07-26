60c1c22f076a367bb18fa215ab0ca4c9b3bfe42f
Youenn Merel <youenn.merel@inria.fr>
2024-07-26 16:21:50 +0200

    [fli-iam#2193] Don't display OHIF viewer for processed datasets

--- shanoir-ng-front/src/app/solr/solr.search.component.ts
+++ shanoir-ng-front/src/app/solr/solr.search.component.ts
@@ -465,7 +465,7 @@ export class SolrSearchComponent implements AfterViewChecked, AfterContentInit {
             {headerName: "Pixel", field: "pixelBandwidth"},
             {headerName: "Mag. strength", field: "magneticFieldStrength"},
             {headerName: "OHIF Viewer", type: "button", awesome: "fa-solid fa-up-right-from-square",
-              condition: item => (item.datasetType.includes("MR") || item.datasetType.includes("Pet") || item.datasetType.includes("Ct")),
+              condition: item => (!item.processed && (item.datasetType.includes("MR") || item.datasetType.includes("Pet") || item.datasetType.includes("Ct"))),
               action: item => {
                 window.open(environment.viewerUrl + '/viewer/1.4.9.12.34.1.8527.' + item.examinationId, '_blank');
               }
