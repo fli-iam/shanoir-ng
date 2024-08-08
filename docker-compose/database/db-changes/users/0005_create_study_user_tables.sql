28ff7b9cd7fb7fdbce25f18ea1815eb252719174
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-08-08 17:13:07 +0200

    shanoir-issue#2248: add FK center

--- docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
+++ docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
@@ -15,7 +15,9 @@ CREATE TABLE `study_user` (
 CREATE TABLE `study_user_center` (
   `center_id` bigint(20) NOT NULL,
   `study_user_id` bigint(20) NOT NULL,
-  KEY `FK8jvoy3dqkninlrimnrb8endp3` (`study_user_id`),
+  KEY `FKbwq3j3rtbtndcifv9l6otdjtt` (`center_id`),
+  KEY `FK8jvoy3dqkninlrimnrb8endp3` (`study_user_id`),  
+  CONSTRAINT `FKbwq3j3rtbtndcifv9l6otdjtt` FOREIGN KEY (`center_id`) REFERENCES `center` (`id`)
   CONSTRAINT `FK8jvoy3dqkninlrimnrb8endp3` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`)
 );
 
