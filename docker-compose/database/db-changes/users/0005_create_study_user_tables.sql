2fe6141666ede140bd80a801729ace8ce91c86e0
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-08-08 16:11:44 +0200

    shanoir-issue#2248: add script to create study_user tables in ms-users

---
+++ docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `study_user` (
+  `confirmed` bit(1) NOT NULL,
+  `receive_new_import_report` bit(1) NOT NULL,
+  `receive_study_user_report` bit(1) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `study_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `study_user_idx` (`study_id`,`user_id`),
+  CONSTRAINT `FKc4ftmuoc0u0ghw43dxth2m8we` FOREIGN KEY (`study_id`) REFERENCES `study` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1
+
+
+CREATE TABLE `study_user_center` (
+  `center_id` bigint(20) NOT NULL,
+  `study_user_id` bigint(20) NOT NULL,
+  KEY `FKbwq3j3rtbtndcifv9l6otdjtt` (`center_id`),
+  KEY `FK8jvoy3dqkninlrimnrb8endp3` (`study_user_id`),
+  CONSTRAINT `FK8jvoy3dqkninlrimnrb8endp3` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`),
+  CONSTRAINT `FKbwq3j3rtbtndcifv9l6otdjtt` FOREIGN KEY (`center_id`) REFERENCES `center` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+CREATE TABLE `study_user_study_user_rights` (
+  `study_user_rights` int(11) DEFAULT NULL,
+  `study_user_id` bigint(20) NOT NULL,
+  KEY `FK6ipbom6lji60h38bd3ok2r098` (`study_user_id`),
+  CONSTRAINT `FK6ipbom6lji60h38bd3ok2r098` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
