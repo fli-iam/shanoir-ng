54d5e6e92beb5e926714bc824f6ca4799e10504a
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-08-12 15:27:56 +0200

    shanoir-issue#2248: another try

--- docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
+++ docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
@@ -1,51 +1,26 @@
-CREATE TABLE study_user
-(
-  confirmed bit NOT NULL,
-  receive_new_import_report bit NOT NULL,
-  receive_study_user_report bit NOT NULL,
-  id bigint PRIMARY KEY NOT NULL,
-  study_id bigint,
-  user_id bigint,
-  user_name varchar(255)
-);
-
-ALTER TABLE study_user
-ADD CONSTRAINT FKc4ftmuoc0u0ghw43dxth2m8we
-FOREIGN KEY (study_id)
-REFERENCES study(id);
-
-CREATE UNIQUE INDEX study_user_idx ON study_user
-(
- study_id,
- user_id
-);
+CREATE TABLE `study_user` (
+  `id` bigint(20) NOT NULL,
+  `receive_anonymization_report` bit(1) NOT NULL,
+  `receive_new_import_report` bit(1) NOT NULL,
+  `study_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `user_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `study_user_idx` (`study_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE study_user_center
-(
-  center_id bigint NOT NULL,
-  study_user_id bigint NOT NULL,
-);
-
-ALTER TABLE study_user_center
-ADD CONSTRAINT FKbwq3j3rtbtndcifv9l6otdjtt
-FOREIGN KEY (center_id)
-REFERENCES center(id);
-
-ALTER TABLE study_user_center
-ADD CONSTRAINT FK8jvoy3dqkninlrimnrb8endp3
-FOREIGN KEY (study_user_id)
-REFERENCES study_user(id);
+CREATE TABLE `study_user_center` (
+  `study_user_id` bigint(20) NOT NULL,
+  `center_id` bigint(20) NOT NULL,
+  KEY `FK8jvoy3dqkninlrimnrb8endp3` (`study_user_id`),
+  CONSTRAINT `FK8jvoy3dqkninlrimnrb8endp3` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-
-CREATE TABLE study_user_study_user_rights
-(
-  study_user_rights int DEFAULT NOT NULL,
-  study_user_id bigint NOT NULL,
-);
-
-ALTER TABLE study_user_study_user_rights
-ADD CONSTRAINT FK6ipbom6lji60h38bd3ok2r098
-FOREIGN KEY (study_user_id)
-REFERENCES study_user(id);
+CREATE TABLE `study_user_study_user_rights` (
+  `study_user_id` bigint(20) NOT NULL,
+  `study_user_rights` int(11) DEFAULT NULL,
+  KEY `FK6ipbom6lji60h38bd3ok2r098` (`study_user_id`),
+  CONSTRAINT `FK6ipbom6lji60h38bd3ok2r098` FOREIGN KEY (`study_user_id`) REFERENCES `study_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
