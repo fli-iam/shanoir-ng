2de4ba85cffca8fd1f831de2069a798a0ae00d8a
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-08-12 11:16:15 +0200

    shanoir-issue#2248: fix sql script to deploy

--- docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
+++ docker-compose/database/db-changes/users/0005_create_study_user_tables.sql
@@ -1,14 +1,23 @@
-CREATE TABLE `study_user` (
-  `confirmed` bit(1) NOT NULL,
-  `receive_new_import_report` bit(1) NOT NULL,
-  `receive_study_user_report` bit(1) NOT NULL,
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `study_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `study_user_idx` (`study_id`,`user_id`),
-  CONSTRAINT `FKc4ftmuoc0u0ghw43dxth2m8we` FOREIGN KEY (`study_id`) REFERENCES `study` (`id`)
+CREATE TABLE study_user
+(
+  confirmed bit NOT NULL,
+  receive_new_import_report bit NOT NULL,
+  receive_study_user_report bit NOT NULL,
+  id bigint PRIMARY KEY NOT NULL,
+  study_id bigint,
+  user_id bigint,
+  user_name varchar(255)
+);
+
+ALTER TABLE study_user
+ADD CONSTRAINT FKc4ftmuoc0u0ghw43dxth2m8we
+FOREIGN KEY (study_id)
+REFERENCES study(id);
+
+CREATE UNIQUE INDEX study_user_idx ON study_user
+(
+ study_id,
+ user_id
 );
 
 
