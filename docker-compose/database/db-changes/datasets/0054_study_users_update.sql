96359a8fe4ffa93ec4afcc1f4f26b4a9d3792a8b
jcomedouteau <jean-come.douteau@univ-grenoble-alpes.fr>
2024-10-03 10:40:01 +0200

    #2422 - study_user bug
    - Mysql script to clean database

--- docker-compose/database/db-changes/datasets/0054_study_users_update.sql
+++ docker-compose/database/db-changes/datasets/0054_study_users_update.sql
@@ -1,11 +1,10 @@
 delete from datasets.study_user;
+delete from datasets.study_user_study_user_rights;
 
 INSERT INTO datasets.study_user
        (id, receive_study_user_report, receive_new_import_report, study_id, user_id, user_name, confirmed)
 SELECT id, receive_study_user_report, receive_new_import_report, study_id, user_id, user_name, confirmed
 FROM studies.study_user ssu;
-
-delete from datasets.study_user_study_user_rights;
 
 INSERT INTO datasets.study_user_study_user_rights
 (study_user_id, study_user_rights)
