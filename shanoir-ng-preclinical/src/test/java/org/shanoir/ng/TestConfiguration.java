235ee6165eef9203ee262b82784d19361935053c
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2024-11-13 11:54:24 +0100

    shanoir-issue#2482: fix preclinical tests

--- shanoir-ng-preclinical/src/test/java/org/shanoir/ng/TestConfiguration.java
+++ shanoir-ng-preclinical/src/test/java/org/shanoir/ng/TestConfiguration.java
@@ -1,7 +1,11 @@
 package org.shanoir.ng;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -12,4 +16,18 @@ public class TestConfiguration {
 	@MockBean
 	private RabbitTemplate rabbitTemplate;
 
+	@Autowired
+	private ConnectionFactory connectionFactory;
+	@Bean(name = "multipleConsumersFactory")
+	public SimpleRabbitListenerContainerFactory multipleConsumersFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setMaxConcurrentConsumers(100);
+		factory.setConcurrentConsumers(10);
+		factory.setStartConsumerMinInterval(100L);
+		factory.setConsecutiveActiveTrigger(1);
+		factory.setAutoStartup(true);
+		factory.setPrefetchCount(1);
+		return factory;
+	}
 }
