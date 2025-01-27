b1ef706012a73abd03251b4c6bc5366afacee984
Duckflip <guewen.hubert@inria.fr>
2025-01-27 10:11:24 +0100

    2388 : VIP files management

---
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/result/exception/ResultHandlerException.java
@@ -0,0 +1,8 @@
+package org.shanoir.ng.vip.result.exception;
+
+public class ResultHandlerException extends Exception{
+
+    public ResultHandlerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
