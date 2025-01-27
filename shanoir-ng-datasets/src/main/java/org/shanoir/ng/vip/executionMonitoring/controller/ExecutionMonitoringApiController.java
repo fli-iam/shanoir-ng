048278cc4fe54c8fb7af9ab51e3bea05c537e4cb
Duckflip <guewen.hubert@inria.fr>
2025-01-27 10:11:24 +0100

    2388 : VIP files management

---
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/executionMonitoring/controller/ExecutionMonitoringApiController.java
@@ -0,0 +1,59 @@
+package org.shanoir.ng.vip.executionMonitoring.controller;
+
+import org.shanoir.ng.vip.executionMonitoring.model.ExecutionMonitoring;
+import org.shanoir.ng.vip.executionMonitoring.service.ExecutionMonitoringService;
+import org.shanoir.ng.vip.executionMonitoring.dto.ExecutionMonitoringDTO;
+import org.shanoir.ng.vip.executionMonitoring.dto.mapper.ExecutionMonitoringMapper;
+import org.shanoir.ng.shared.error.FieldErrorMap;
+import org.shanoir.ng.shared.exception.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author KhalilKes
+ */
+@Controller
+public class ExecutionMonitoringApiController implements ExecutionMonitoringApi {
+
+
+    @Autowired
+    private ExecutionMonitoringService executionMonitoringService;
+
+    @Autowired
+    private ExecutionMonitoringMapper mapper;
+
+    @Override
+    public ResponseEntity<ExecutionMonitoringDTO> findExecutionMonitoringById(Long executionMonitoringId) {
+        final Optional<ExecutionMonitoring> monitoring = executionMonitoringService.findById(executionMonitoringId);
+        if (monitoring.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(mapper.executionMonitoringToExecutionMonitoringDTO(monitoring.get()), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<ExecutionMonitoringDTO>> getAllExecutionMonitoring() {
+        final List<ExecutionMonitoring> monitorings = executionMonitoringService.findAllAllowed();
+        if (monitorings.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(mapper.executionMonitoringsToExecutionMonitoringDTOs(monitorings), HttpStatus.OK);
+    }
+
+    private void validate(BindingResult result) throws RestServiceException {
+        final FieldErrorMap errors = new FieldErrorMap(result);
+        if (!errors.isEmpty()) {
+            ErrorModel error = new ErrorModel(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Bad arguments",
+                    new ErrorDetails(errors));
+            throw new RestServiceException(error);
+        }
+    }
+
+
+}
