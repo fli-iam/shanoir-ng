dd50affac0f4f2c56eca2fd7e9eb002822990b77
Alexandre Pron <alexandre.pron@gmail.com>
2025-02-05 18:21:01 +0100

    [BF]: parse correctly scientific notations into metadata .json file

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/BidsImporterService.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/BidsImporterService.java
@@ -35,6 +35,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -182,7 +183,6 @@ public class BidsImporterService {
 		float progress = 0f;
 
 		File[] filesToImport = new File(importJob.getWorkFolder()).listFiles(new FilenameFilter() {
-			@Override
 			public boolean accept(File arg0, String name) {
 				return !name.startsWith(".DS_Store") && !name.startsWith("__MAC") && !name.startsWith("._") && !name.startsWith(".AppleDouble");
 			}});
@@ -258,11 +258,11 @@ public class BidsImporterService {
 			files.add(dsFile);
 			if(equipmentId == 0L && importedFile.getName().endsWith(".json") && Files.size(Path.of(importedFile.getPath())) < 1000000) {
 				// Check equipment in json file
-				JSONParser json = new JSONParser(new FileReader(importedFile));
-				LinkedHashMap jsonObject = (LinkedHashMap) json.parse();
-				ObjectMapper objectMapper = new ObjectMapper();
+				//JSONParser json = new JSONParser(new FileReader(importedFile));
+				// LinkedHashMap jsonObject = (LinkedHashMap) json.parse();
+				ObjectMapper jsonMapper = new ObjectMapper();
 				// Parse JSON file into a LinkedHashMap
-				LinkedHashMap<String, Object> jsonObject = objectMapper.readValue(importedFile, LinkedHashMap.class);
+				LinkedHashMap<String, Object> jsonObject = jsonMapper.readValue(importedFile, LinkedHashMap.class);
 				if (jsonObject.get("DeviceSerialNumber") != null) {
 					String code = (String) jsonObject.get("DeviceSerialNumber");
 					equipmentId = equipments.get(code) != null ? Long.valueOf(equipments.get(code)) : 0L;
