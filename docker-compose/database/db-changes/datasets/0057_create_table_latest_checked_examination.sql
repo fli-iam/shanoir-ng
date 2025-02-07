4f0d01659794f06132dd1a7b0e6c818944479a62
michaelkain <michael.kain@inria.fr>
2025-02-07 14:52:12 +0100

    Fusion all in one sql for db-changes

--- docker-compose/database/db-changes/datasets/0057_create_table_latest_checked_examination.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE latest_checked_examination (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    examination_id BIGINT NOT NULL
-);
--- docker-compose/database/db-changes/datasets/0058_alter_table_examination_add_study_instance_uid.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `examination` ADD COLUMN `study_instance_uid` varchar(255);
-
-CREATE UNIQUE INDEX UK_15imoe7p5ks3na50kltrhvw6r ON examination(study_instance_uid);
