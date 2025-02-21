62ce5aa7b79ad987188828c3cb91fed7206d7a35
LaurentPV <laurentpaulvallet@gmail.com>
2025-02-21 14:27:13 +0100

    fix sql script as pet_protocol already has number_of_slices

--- docker-compose/database/db-changes/datasets/0057_add_fields_to_model.sql
+++ docker-compose/database/db-changes/datasets/0057_add_fields_to_model.sql
@@ -6,7 +6,6 @@ ALTER TABLE xa_protocol ADD COLUMN slice_thickness double default null;
 
 ALTER TABLE ct_protocol ADD COLUMN number_of_slices int default null;
 ALTER TABLE mr_protocol ADD COLUMN number_of_slices int default null;
-ALTER TABLE pet_protocol ADD COLUMN number_of_slices int default null;
 ALTER TABLE xa_protocol ADD COLUMN number_of_slices int default null;
 
 
