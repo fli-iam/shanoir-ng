15ad37ffb6ea1bd1bf943f355046e62e53e5cfac
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2025-03-04 15:29:55 +0100

    shanoir-issue#2675: call deleteFiles in one sql call

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/datasetfile/DatasetFileRepository.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/datasetfile/DatasetFileRepository.java
@@ -1,6 +1,16 @@
 package org.shanoir.ng.datasetfile;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DatasetFileRepository extends CrudRepository<DatasetFile, Long> {
+
+    @Modifying
+    @Query("DELETE FROM DatasetFile df WHERE df.datasetExpression.id IN :expressionIds")
+    void deleteByDatasetExpressionIds(@Param("expressionIds") List<Long> expressionIds);
+
 }
