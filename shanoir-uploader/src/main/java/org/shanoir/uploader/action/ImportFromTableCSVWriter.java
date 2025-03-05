5b0959996de99d84df2d7286e44bfbe46f0a72ef
michaelkain <michael.kain@inria.fr>
2025-03-05 16:49:48 +0100

    MassImportCSVWriter added

---
+++ shanoir-uploader/src/main/java/org/shanoir/uploader/action/ImportFromTableCSVWriter.java
@@ -0,0 +1,76 @@
+package org.shanoir.uploader.action;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.shanoir.uploader.ShUpOnloadConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opencsv.CSVWriter;
+
+public class ImportFromTableCSVWriter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ImportFromTableCSVWriter.class);
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+
+    private static final String MASS_IMPORT_EXAMINATIONS_OK = "mass_import_examinations_OK";
+
+    private static final String MASS_IMPORT_EXAMINATIONS_KO = "mass_import_examinations_KO";
+
+    private static final String CSV = ".csv";
+
+    private static final String[] HEADER = {"FirstName", "LastName", "BirthName", "BirthDate",
+        "SubjectName", "ExaminationDate", "Error"};
+
+    private File examinationsOK;
+
+    private File examinationsKO;
+
+    public ImportFromTableCSVWriter() {
+        File workFolder = ShUpOnloadConfig.getWorkFolder();
+        Date now = new Date();
+        String timestamp = sdf.format(now);
+        String csvFileName = MASS_IMPORT_EXAMINATIONS_OK + timestamp + CSV;
+        this.examinationsOK = createCSVFile(workFolder, csvFileName);
+        csvFileName = MASS_IMPORT_EXAMINATIONS_KO + timestamp + CSV;
+        this.examinationsKO = createCSVFile(workFolder, csvFileName);
+    }
+
+    private File createCSVFile(File workFolder, String csvFileName) {
+        File csvFile = new File(workFolder.getAbsolutePath() + File.separator + csvFileName);
+        if (!csvFile.exists()) {
+            try {
+                csvFile.createNewFile();
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+        // Create header by default
+        try (CSVWriter writer = new CSVWriter(new FileWriter(csvFile))) {
+            writer.writeNext(HEADER);
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return csvFile;
+    }
+
+    public void addExaminationLine(boolean ok, String[] line) {
+        File csvFile;
+        if (ok) {
+            csvFile = examinationsOK;
+        } else {
+            csvFile = examinationsKO;
+        }
+        try (CSVWriter writer = new CSVWriter(new FileWriter(csvFile))) {
+            writer.writeNext(line);
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+}
