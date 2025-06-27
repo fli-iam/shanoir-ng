91ea2b83894f791fab9c77c3849aae085765f3cc
LaurentPV <laurentpaulvallet@gmail.com>
2025-06-27 12:12:39 +0200

    bugfix big5 charset exception

--- shanoir-uploader/src/main/java/org/shanoir/uploader/ShUpConfig.java
+++ shanoir-uploader/src/main/java/org/shanoir/uploader/ShUpConfig.java
@@ -20,7 +20,7 @@ public class ShUpConfig {
 	 */
 	public static final String SHANOIR_UPLOADER_VERSION = "v2.6.5";
 	
-	public static final String RELEASE_DATE = "2025-04-29";
+	public static final String RELEASE_DATE = "2025-06-27"; // Do we keep it ?
 	
 	public static final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 	
