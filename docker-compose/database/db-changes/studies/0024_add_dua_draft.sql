564e4b13b881bb19bddf4160ac4dde9e57e03d96
Julien Louis <jul.louis@gmail.com>
2025-06-30 16:29:33 +0200

    fix sql typo

--- docker-compose/database/db-changes/studies/0024_add_dua_draft.sql
+++ docker-compose/database/db-changes/studies/0024_add_dua_draft.sql
@@ -1,6 +1,7 @@
 create table dua_draft (
     id varchar(36) not null,
     study_id bigint(20) not null,
+    study_name varchar(255) not null,
     url tinytext,
     funding text,
     thanks text,
