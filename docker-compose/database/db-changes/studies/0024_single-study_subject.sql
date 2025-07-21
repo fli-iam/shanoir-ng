f53181853bcccd4341cafe6148f58af893c8b28c
michaelkain <michael.kain@inria.fr>
2025-07-21 12:20:22 +0200

    Final migration script

--- docker-compose/database/db-changes/studies/0024_single-study_subject.sql
+++ docker-compose/database/db-changes/studies/0024_single-study_subject.sql
@@ -12,12 +12,64 @@ ALTER TABLE subject
     ADD COLUMN subject_type INT(11),
     ADD COLUMN quality_tag INT(11);
 
-# Copy columns from subject_study into subject
+# Copy columns from subject_study into subject (single-study subject already)
 UPDATE subject s
-JOIN subject_study ss ON s.id = ss.subject_id
+JOIN (
+    SELECT subject_id,
+           MAX(subject_study_identifier) AS subject_study_identifier,
+           MAX(physically_involved) AS physically_involved,
+           MAX(subject_type) AS subject_type,
+           MAX(quality_tag) AS quality_tag,
+           MAX(study_id) AS study_id
+    FROM subject_study
+    GROUP BY subject_id
+    HAVING COUNT(*) = 1
+) ss ON s.id = ss.subject_id
 SET
-    s.study_id = ss.study_id,
     s.study_identifier = ss.subject_study_identifier,
     s.physically_involved = ss.physically_involved,
     s.subject_type = ss.subject_type,
-    s.quality_tag = ss.quality_tag;
+    s.quality_tag = ss.quality_tag,
+    s.study_id = ss.study_id;
+
+# Add new subject, in case multi-study subject
+INSERT INTO subject (
+    birth_date,
+    identifier,
+    imaged_object_category,
+    language_hemispheric_dominance,
+    manual_hemispheric_dominance,
+    name,
+    preclinical,
+    sex,
+    pseudonymus_hash_values_id,
+    study_identifier,
+    physically_involved,
+    subject_type,
+    quality_tag,
+    study_id
+)
+SELECT
+    s.birth_date,
+    s.identifier,
+    s.imaged_object_category,
+    s.language_hemispheric_dominance,
+    s.manual_hemispheric_dominance,
+    s.name,
+    s.preclinical,
+    s.sex,
+    s.pseudonymus_hash_values_id,
+    ss.subject_study_identifier,
+    ss.physically_involved,
+    ss.subject_type,
+    ss.quality_tag,
+    ss.study_id
+FROM subject_study ss
+JOIN (
+    SELECT subject_id
+    FROM subject_study
+    GROUP BY subject_id
+    HAVING COUNT(*) > 1
+) multi ON ss.subject_id = multi.subject_id
+JOIN subject s ON ss.subject_id = s.id;
+
