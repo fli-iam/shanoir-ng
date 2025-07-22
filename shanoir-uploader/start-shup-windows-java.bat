7d324e79e055275e8c2582b72d81e42170a5db25
LaurentPV <laurentpaulvallet@gmail.com>
2025-07-22 14:07:49 +0200

    bugfix syntax windows bash script

--- shanoir-uploader/start-shup-windows-java.bat
+++ shanoir-uploader/start-shup-windows-java.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+setlocal enabledelayedexpansion
 
 FOR %%f in (shanoir-uploader-*-jar-with-dependencies.jar) do (
     set "JAR_FILE=%%f"
@@ -8,6 +9,7 @@ FOR %%f in (shanoir-uploader-*-jar-with-dependencies.jar) do (
 ECHO ERROR: No JAR file found with the pattern shanoir-uploader-*-jar-with-dependencies.jar
 EXIT /b 1
 
+:found
 IF EXIST "%JAVA_HOME%\bin\javaw.exe" (
 	ECHO Starting ShanoirUploader using JAVA_HOME...
 	"%JAVA_HOME%\bin\javaw.exe" -Dhttps.protocols=TLSv1.2 -Xms512m -Xmx2g -Xnoclassgc -jar "%JAR_FILE%" org.shanoir.uploader.ShanoirUploader  
