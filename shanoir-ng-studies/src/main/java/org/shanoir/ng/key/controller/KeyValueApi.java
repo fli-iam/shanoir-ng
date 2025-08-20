2c12c4b023aec4bc1011e96a1f3a6dd4783e625e
michaelkain <michael.kain@inria.fr>
2025-08-20 09:42:58 +0200

    Add KeyValueApi to MS Studies

---
+++ shanoir-ng-studies/src/main/java/org/shanoir/ng/key/controller/KeyValueApi.java
@@ -0,0 +1,42 @@
+package org.shanoir.ng.key.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "key", description = "the key value API")
+@RequestMapping("/key")
+public interface KeyValueApi {
+
+    @Operation(
+            summary = "Get value by key",
+            description = "Returns the value for the given key, if present"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Value found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(type = "string", example = "abc123hash")
+            )
+            ),
+        @ApiResponse(responseCode = "204", description = "No value found for the key"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized"),
+        @ApiResponse(responseCode = "403", description = "Forbidden"),
+        @ApiResponse(responseCode = "500", description = "Unexpected error")
+    })
+    @GetMapping(value = "/{key}", produces = "application/json")
+    @PreAuthorize("hasAnyRole('ADMIN','EXPERT','USER')")
+    ResponseEntity<String> findValue(@PathVariable String key);
+
+}
