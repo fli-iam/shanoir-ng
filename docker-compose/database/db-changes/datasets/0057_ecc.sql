450eaf3821dc8783b9164ceca85953b7a5e170e3
michaelkain <michael.kain@inria.fr>
2025-09-03 17:00:34 +0200

    Remove unique keys for old-prod-data

--- docker-compose/database/db-changes/datasets/0057_ecc.sql
+++ docker-compose/database/db-changes/datasets/0057_ecc.sql
@@ -5,9 +5,4 @@ CREATE TABLE examination_last_checked (
 
 ALTER TABLE `examination` ADD COLUMN `study_instance_uid` varchar(255);
 
-CREATE UNIQUE INDEX UK_15imoe7p5ks3na50kltrhvw6r ON examination(study_instance_uid);
-
 ALTER TABLE `dataset_acquisition` ADD COLUMN `series_instance_uid` varchar(255);
-
-CREATE UNIQUE INDEX UK_13imoe4p5ks3na77kltrhvw9r ON dataset_acquisition(series_instance_uid);
-
