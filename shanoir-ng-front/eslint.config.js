33d2e6ae391cbdcb08ddad4a51abfecd1ca8af78
Julien Louis <jul.louis@gmail.com>
2025-09-25 16:26:14 +0200

    eslint no-empty

--- shanoir-ng-front/eslint.config.js
+++ shanoir-ng-front/eslint.config.js
@@ -46,7 +46,6 @@ module.exports = tseslint.config(
       ],
 
       // Core ESLint rules - turn off common errors
-      "no-empty": "off",
       "no-irregular-whitespace": "off",
       "no-self-assign": "off",
       "no-extra-boolean-cast": "off",
