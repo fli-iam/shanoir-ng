57537719d97d6c28cdeac7d1b3945c712cac6e8b
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2025-10-06 14:52:28 +0200

    shanoir-issue#3008: fix duplicate import

--- shanoir-ng-front/src/app/acquisition-equipments/manufacturer/manufacturer.component.ts
+++ shanoir-ng-front/src/app/acquisition-equipments/manufacturer/manufacturer.component.ts
@@ -15,13 +15,10 @@
 import { Component } from '@angular/core';
 import { UntypedFormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-
 import { EntityService } from 'src/app/shared/components/entity/entity.abstract.service';
-
 import { EntityComponent } from '../../shared/components/entity/entity.component.abstract';
 import { Manufacturer } from '../shared/manufacturer.model';
 import { ManufacturerService } from '../shared/manufacturer.service';
-import { EntityService } from 'src/app/shared/components/entity/entity.abstract.service';
 
 @Component({
     selector: 'manufacturer-detail',
