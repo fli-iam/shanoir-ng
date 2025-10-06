9dac78d311d68c245e791ccce89305fc4693c285
Julien Louis <jul.louis@gmail.com>
2025-10-06 12:14:28 +0200

    change columns type

---
+++ docker-compose/database/db-changes/studies/0027_dua_draft_col_types.sql
@@ -0,0 +1,4 @@
+-- modify columns types in dua_draft
+ALTER TABLE dua_draft MODIFY COLUMN funding text NULL;
+ALTER TABLE dua_draft MODIFY COLUMN thanks text NULL;
+ALTER TABLE dua_draft MODIFY COLUMN papers text NULL;
