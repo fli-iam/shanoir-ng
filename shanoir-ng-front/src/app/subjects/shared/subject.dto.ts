2c181bc4e42eebb76ae1aed957c36fdd9a35b20d
pierrehenri-dauvergne <pierre-henri.dauvergne@inria.fr>
2025-10-20 17:13:59 +0200

    remove unused import

--- shanoir-ng-front/src/app/subjects/shared/subject.dto.ts
+++ shanoir-ng-front/src/app/subjects/shared/subject.dto.ts
@@ -22,7 +22,6 @@ import { Study } from "../../studies/shared/study.model";
 import { ImagedObjectCategory } from './imaged-object-category.enum';
 import { Subject } from './subject.model';
 import { Sex, SubjectType } from './subject.types';
-import {Field} from "../../shared/reflect/field.decorator";
 
 
 @Injectable()
