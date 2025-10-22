489111f76d2a432ed1ec94581edc4bad1f441d4c
michaelkain <michael.kain@inria.fr>
2025-10-22 14:36:09 +0200

    Update ImporterServiceTest.java

--- shanoir-ng-datasets/src/test/java/org/shanoir/ng/datasetacquisition/service/ImporterServiceTest.java
+++ shanoir-ng-datasets/src/test/java/org/shanoir/ng/datasetacquisition/service/ImporterServiceTest.java
@@ -60,7 +60,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class ImporterServiceTest {
