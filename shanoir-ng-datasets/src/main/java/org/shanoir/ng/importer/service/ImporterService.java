cfc06d2b9fd0e9196b159886611c27c886ae44ba
michaelkain <michael.kain@inria.fr>
2025-10-24 10:28:10 +0200

    Adding code for retro-compatibility, that all StudyInstanceUIDs are the same

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/ImporterService.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/ImporterService.java
@@ -21,6 +21,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.dcm4che3.data.Tag;
 import org.shanoir.ng.datasetacquisition.model.DatasetAcquisition;
 import org.shanoir.ng.datasetacquisition.service.DatasetAcquisitionService;
 import org.shanoir.ng.dicom.DicomProcessing;
@@ -55,6 +56,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import ch.qos.logback.core.util.StringUtil;
 
 @Service
 @Scope("prototype")
@@ -245,6 +248,8 @@ public class ImporterService {
                     } catch (PacsException e) {
                         throw new ShanoirException("Unable to retrieve dicom attributes in file " + serie.getFirstDatasetFileForCurrentSerie().getPath(), e);
                     }
+
+                    manageStudyInstanceUIDs(examination, importJob, dicomAttributes);
                     
                     // Generate acquisition object with all sub objects : datasets, protocols, expressions, ...
                     DatasetAcquisition acquisition = createDatasetAcquisitionForSerie(serie, rank, examination, importJob, dicomAttributes);
@@ -268,6 +273,42 @@ public class ImporterService {
             }
         }
         return generatedAcquisitions;
+    }
+
+    /**
+     * This method has been added for retro-compatibility and
+     * can be removed in the future. Old versions of ShUp will
+     * not use the new generated StudyInstanceUID during the
+     * creation of the exam, they will pseudonymize and produce
+     * a new random StudyInstanceUID. This code corrects this gap
+     * that all data are all in line. Old version of ShUp will not
+     * send a StudyInstanceUID in their ImportJob.
+     * 
+     * @param examination
+     * @param importJob
+     * @param dicomAttributes
+     * @throws ShanoirException
+     */
+    private void manageStudyInstanceUIDs(Examination examination, ImportJob importJob,
+            AcquisitionAttributes<String> dicomAttributes) throws ShanoirException {
+        String studyInstanceUIDDICOM = dicomAttributes.getFirstDatasetAttributes().getString(Tag.StudyInstanceUID);
+        String studyInstanceUIDExamination = examination.getStudyInstanceUID();
+        // Only transmitted by new version of ShUp:
+        String studyInstanceUIDImportJob = importJob.getStudyInstanceUID();
+        if (StringUtil.isNullOrEmpty(studyInstanceUIDImportJob)) { // Handle old versions of ShUp: nothing in ImportJob
+            if (StringUtil.isNullOrEmpty(studyInstanceUIDExamination)
+                || !examination.getStudyInstanceUID().equals(studyInstanceUIDDICOM)) {
+                LOG.info("Old version of ShUp used: updating StudyInstanceUID of examination from {} to {}", examination.getStudyInstanceUID(), studyInstanceUIDDICOM);
+                examinationRepository.updateStudyInstanceUID(examination.getId(), studyInstanceUIDDICOM);
+            } // do nothing as all in line
+        } else { // New version of ShUp, sending in ImportJob
+            if (examination.getStudyInstanceUID().equals(studyInstanceUIDDICOM)
+                && examination.getStudyInstanceUID().equals(studyInstanceUIDImportJob)) {
+                // do nothing as all in line
+            } else {
+                throw new ShanoirException("Error with StudyInstanceUIDs.");
+            }
+        }
     }
 
     StudyCard getStudyCard(ImportJob importJob) {
