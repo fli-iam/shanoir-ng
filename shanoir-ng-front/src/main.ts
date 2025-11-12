7ede05ecc372a3db17c430c54c4e6e4c24f3af05
Julien Louis <jul.louis@gmail.com>
2025-11-12 09:29:54 +0100

    remove unused files + migrate deprecated platformBrowserDynamic

--- shanoir-ng-front/src/main.ts
+++ shanoir-ng-front/src/main.ts
@@ -1,16 +1,13 @@
-import { Buffer } from 'buffer';
-(window as any).Buffer = Buffer;
-
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
 import localeDe from '@angular/common/locales/de';
 import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
+import { AppModule } from './app/app.module';
 import { KeycloakService } from './app/shared/keycloak/keycloak.service';
 import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
 
 registerLocaleData(localeFr);
 registerLocaleData(localeDe);
@@ -25,18 +22,18 @@ if (window.location.href == window.origin + '/shanoir-ng/'
         || window.location.href.endsWith('/extension-request')
         || window.location.href.endsWith('/challenge-request')) {
     // Public URL
-    platformBrowserDynamic().bootstrapModule(AppModule);
+    bootstrapApplication(AppModule);
 } else if (window.location.href.includes('/dua/edit')
         || window.location.href.includes('/dua/view')) {
     // Can be either public or private
     KeycloakService.init(true)
     .then(() => {
-        platformBrowserDynamic().bootstrapModule(AppModule);
+        bootstrapApplication(AppModule);
     });
 } else {
     // private 
     KeycloakService.init()
         .then(() => {
-            platformBrowserDynamic().bootstrapModule(AppModule);
+            bootstrapApplication(AppModule);
         });
 }
