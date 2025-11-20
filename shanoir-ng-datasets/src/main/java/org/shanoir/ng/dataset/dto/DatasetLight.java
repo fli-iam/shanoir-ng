af4dabca6c425f5d9cd3df3b8a9ea40b8e887ae8
Julien Louis <jul.louis@gmail.com>
2025-11-20 11:44:09 +0100

    fix ds tables in processing view

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/dto/DatasetLight.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/dataset/dto/DatasetLight.java
@@ -2,12 +2,12 @@
  * Shanoir NG - Import, manage and share neuroimaging data
  * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
  * Contact us on https://project.inria.fr/shanoir/
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -15,29 +15,48 @@
 package org.shanoir.ng.dataset.dto;
 
 import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
 
 import org.shanoir.ng.dataset.model.Dataset;
+import org.shanoir.ng.shared.core.model.IdName;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 public class DatasetLight {
 
 	private Long id;
-	
+
 	private String name;
-	
+
 	private String type;
 
 	private boolean hasProcessings;
 
-	private Long studyId;
+	private IdName study;
+
+	private IdName subject;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Europe/Paris")
+	private LocalDate creationDate;
 
 
-    public DatasetLight(Long id, String name, Class<? extends Dataset> type, Long studyId, boolean hasProcessings) throws NoSuchMethodException, InstantiationException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+    public DatasetLight(
+			Long id,
+			String name,
+			Class<? extends Dataset> type,
+			Long studyId, String studyName,
+			Long subjectId, String subjectName,
+			LocalDate creationDate,
+			boolean hasProcessings
+	) throws NoSuchMethodException,InstantiationException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
         this.hasProcessings = hasProcessings;
         this.id = id;
         this.name = name;
         this.type = type.getDeclaredConstructor().newInstance().getType().name();
-		this.studyId = studyId;
+		this.study = new IdName(studyId, studyName);
+		this.subject = new IdName(subjectId, subjectName);
+		this.creationDate = creationDate;
     }
 
 	public Long getId() {
@@ -72,11 +91,27 @@ public class DatasetLight {
         this.hasProcessings = hasProcessings;
     }
 
-    public Long getStudyId() {
-        return studyId;
-    }
+	public IdName getStudy() {
+		return study;
+	}
 
-    public void setStudyId(Long studyId) {
-        this.studyId = studyId;
-    }
+	public void setStudy(IdName study) {
+		this.study = study;
+	}
+
+	public IdName getSubject() {
+		return subject;
+	}
+
+	public void setSubject(IdName subject) {
+		this.subject = subject;
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
 }
