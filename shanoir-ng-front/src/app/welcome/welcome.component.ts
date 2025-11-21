389b8ac01db4949cdcff8d7e0fb737a2d4d9787f
Julien Louis <jul.louis@gmail.com>
2025-11-21 16:02:38 +0100

    fix ts

--- shanoir-ng-front/src/app/welcome/welcome.component.ts
+++ shanoir-ng-front/src/app/welcome/welcome.component.ts
@@ -1,28 +1,16 @@
-/**
- * Shanoir NG - Import, manage and share neuroimaging data
- * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
- * Contact us on https://project.inria.fr/shanoir/
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
- */
 
-import { DOCUMENT } from "@angular/common";
 import {
-    Component,
-    ElementRef,
-    HostListener,
-    Inject,
-    OnInit,
-    Renderer2,
-    ViewChild,
-    ViewEncapsulation
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation,
+  DOCUMENT
 } from '@angular/core';
+
 import { ConfirmDialogService } from '../shared/components/confirm-dialog/confirm-dialog.service';
 import { ImagesUrlUtil } from "../shared/utils/images-url.util";
 import { StudyType } from "../studies/shared/study-type.enum";
@@ -60,11 +48,11 @@ export class WelcomeComponent implements OnInit {
     }
 
     addSchemaToDOM(): void {
-        let script = this._renderer2.createElement('script');
+        const script = this._renderer2.createElement('script');
         script.type = `application/ld+json`;
 
         let datasetStr: string = "";
-        let shanoirUrl: string = window.location.protocol + "//" + window.location.hostname;
+        const shanoirUrl: string = window.location.protocol + "//" + window.location.hostname;
 
         this.studies?.forEach( study => {
 
@@ -198,8 +186,8 @@ export class WelcomeComponent implements OnInit {
 	}
 
 	@HostListener('window:scroll', ['$event']) onWindowScroll(e) {
-		let scroll = e.target['scrollingElement'].scrollTop + window.innerHeight;
-		let end = this.showMore?.nativeElement?.offsetTop;
+		const scroll = e.target['scrollingElement'].scrollTop + window.innerHeight;
+		const end = this.showMore?.nativeElement?.offsetTop;
 		if (scroll > end && this.studies.length > this.show) this.increaseShow();
 	}
 }
--- shanoir-ng-front/src/version.ts
+++ shanoir-ng-front/src/version.ts
@@ -1,21 +1,11 @@
-/**
- * Shanoir NG - Import, manage and share neuroimaging data
- * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
- * Contact us on https://project.inria.fr/shanoir/
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
- */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { resolve, relative } = require('path');
 
 const { gitDescribeSync } = require('git-describe');
-const { version } = require('../package.json');
-const { resolve, relative } = require('path');
 const { writeFileSync } = require('fs-extra');
+
+const { version } = require('../package.json');
+
 
 const gitInfo = gitDescribeSync({
     match: 'NG_v[0-9]*',
