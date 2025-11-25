20b57c4629e362f7ceab72fb5ad48f4c908dbf79
Julien Louis <jul.louis@gmail.com>
2025-11-25 14:43:04 +0100

    checkstyle commons

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/dicom/DicomUtils.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/dicom/DicomUtils.java
@@ -1,3 +1,17 @@
+/**
+ * Shanoir NG - Import, manage and share neuroimaging data
+ * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
+ * Contact us on https://project.inria.fr/shanoir/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 package org.shanoir.ng.shared.dicom;
 
 import java.io.File;
@@ -10,7 +24,7 @@ import org.dcm4che3.dcmr.AcquisitionModality;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DicomUtils {
+public final class DicomUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(DicomUtils.class);
 
@@ -19,6 +33,8 @@ public class DicomUtils {
     private static final String RTDOSE = "RTDOSE";
 
     private static final String RTSTRUCT = "RTSTRUCT";
+
+    private DicomUtils() { }
 
     /**
      * Ignore all series, that are not medical imaging.
