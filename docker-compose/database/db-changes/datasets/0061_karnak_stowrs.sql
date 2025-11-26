2d56c1dec4e27643d6e525d825c2a8b588e5d072
michaelkain <michael.kain@inria.fr>
2025-11-26 14:48:25 +0100

    Update 0061_karnak_stowrs.sql

--- docker-compose/database/db-changes/datasets/0061_karnak_stowrs.sql
+++ docker-compose/database/db-changes/datasets/0061_karnak_stowrs.sql
@@ -1,19 +1,19 @@
 ALTER TABLE `dataset_acquisition` ADD COLUMN `acquisition_number` INT(11);
 
-ALTER TABLE `dataset_metadata` ADD COLUMN `image_orientation_patient` varchar(255);
+ALTER TABLE `dataset_metadata` ADD COLUMN `image_orientation_patient` VARCHAR(255);
 
 CREATE INDEX idx_dataset_acquisition_series_exam 
 ON dataset_acquisition(series_instance_uid, examination_id);
 
 CREATE TABLE study_center (
-  id bigint(20) NOT NULL,
-  study_id bigint(20) NOT NULL,
-  center_id bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK2hmmh3c0w1tk8npi87hpvf10i` (`study_id`),
-  KEY `FKi5ioon77o40h52tyxdqubfpdp` (`center_id`),
-  CONSTRAINT `FK2hmmh3c0w1tk8npi87hpvf10i` FOREIGN KEY (`study_id`) REFERENCES `study` (`id`),
-  CONSTRAINT `FKi5ioon77o40h52tyxdqubfpdp` FOREIGN KEY (`center_id`) REFERENCES `center` (`id`)
+  id BIGINT(20) NOT NULL,
+  study_id BIGINT(20) NOT NULL,
+  center_id BIGINT(20) NOT NULL,
+  PRIMARY KEY (id),
+  KEY `FK2hmmh3c0w1tk8npi87hpvf10i` (study_id),
+  KEY `FKi5ioon77o40h52tyxdqubfpdp` (center_id),
+  CONSTRAINT `FK2hmmh3c0w1tk8npi87hpvf10i` FOREIGN KEY (study_id) REFERENCES `study` (id),
+  CONSTRAINT `FKi5ioon77o40h52tyxdqubfpdp` FOREIGN KEY (center_id) REFERENCES `center` (id)
 );
 
 # Replicate study_center into MS Datasets
@@ -21,4 +21,4 @@ INSERT INTO study_center (id, study_id, center_id)
 SELECT
   id, study_id, center_id
 FROM 
-    studies.study_center ssc;
+  studies.study_center ssc;
