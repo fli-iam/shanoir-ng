36570492e0ccad53bd1fa087367178ef08314008
michaelkain <michael.kain@inria.fr>
2025-12-05 17:17:59 +0100

    Update ExaminationsConsistencyChecker.java

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/schedule/ExaminationsConsistencyChecker.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/schedule/ExaminationsConsistencyChecker.java
@@ -37,7 +37,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.opencsv.CSVWriter;
