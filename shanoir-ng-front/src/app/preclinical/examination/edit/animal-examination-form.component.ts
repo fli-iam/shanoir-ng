2be43fbe5f7866e9856ac33169620e9df5ddce6e
Adam Fragkiadakis <adamfrag@gmail.com>
2025-12-16 14:44:06 +0100

    Fix TS styles

--- shanoir-ng-front/src/app/preclinical/examination/edit/animal-examination-form.component.ts
+++ shanoir-ng-front/src/app/preclinical/examination/edit/animal-examination-form.component.ts
@@ -12,7 +12,7 @@
  * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
  */
 
-import { NgModule, Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { UntypedFormGroup, Validators } from '@angular/forms';
 import { HttpResponse } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
