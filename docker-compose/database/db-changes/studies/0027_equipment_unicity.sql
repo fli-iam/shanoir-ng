73304b69733677de80f6200edfd02a1df82f2bfa
Julien Louis <jul.louis@gmail.com>
2025-12-22 11:28:02 +0100

    fix sql + constraint name

--- docker-compose/database/db-changes/studies/0027_equipment_unicity.sql
+++ docker-compose/database/db-changes/studies/0027_equipment_unicity.sql
@@ -1,7 +1,8 @@
 -- drop and recreate unique constraint on acquisition_equipment to include center_id
 ALTER TABLE acquisition_equipment
-    DROP FOREIGN KEY xxx,
-    DROP INDEX model_number_idx,
+    DROP FOREIGN KEY FKbvbig13gxsu8gxaw9h6uemhk4,
+    DROP INDEX model_number_idx;
+ALTER TABLE acquisition_equipment
     ADD UNIQUE KEY model_number_idx (center_id, manufacturer_model_id, serial_number),
-    ADD CONSTRAINT xxx
+    ADD CONSTRAINT FKbvbig13gxsu8gxaw9h6uemhk4
         FOREIGN KEY (manufacturer_model_id) REFERENCES manufacturer_model(id);
