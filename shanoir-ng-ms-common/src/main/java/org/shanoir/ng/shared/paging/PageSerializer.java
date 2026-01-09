fe85924d7b34db73fda65b49c69acf9a105bdbc4
michaelkain <michael.kain@inria.fr>
2026-01-09 13:33:14 +0100

    Fix for PageSerializer

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/paging/PageSerializer.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/paging/PageSerializer.java
@@ -16,14 +16,14 @@ package org.shanoir.ng.shared.paging;
 
 import java.io.IOException;
 
-import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.boot.jackson.JacksonComponent;
 import org.springframework.data.domain.PageImpl;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
-@JsonComponent
+@JacksonComponent
 public class PageSerializer extends StdSerializer<PageImpl> {
 
     private static final long serialVersionUID = 1L;
