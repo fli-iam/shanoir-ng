2c0db0191a53b657090208e1e04e611c20de29e3
Duckflip <guewen.hubert@inria.fr>
2026-02-11 14:14:33 +0100

    2698 (real 2712) : API for post processing VIP execution result
    # Conflicts:
    #	shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/output/service/OutputService.java
    
    # Conflicts:
    #	shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/output/handler/OFSEPSeqIdHandler.java

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/executionMonitoring/service/ExecutionMonitoringServiceImpl.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/vip/executionMonitoring/service/ExecutionMonitoringServiceImpl.java
@@ -32,7 +32,7 @@ import org.shanoir.ng.vip.executionMonitoring.security.ExecutionMonitoringSecuri
 import org.shanoir.ng.shared.exception.EntityNotFoundException;
 import org.shanoir.ng.utils.Utils;
 import org.shanoir.ng.vip.output.exception.ResultHandlerException;
-import org.shanoir.ng.vip.output.service.OutputService;
+import org.shanoir.ng.vip.output.service.OutputServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -82,7 +82,7 @@ public class ExecutionMonitoringServiceImpl implements ExecutionMonitoringServic
     private ExecutionServiceImpl executionService;
 
     @Autowired
-    private OutputService outputService;
+    private OutputServiceImpl outputService;
 
     @Autowired
     @Lazy
