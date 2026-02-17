c41a7497e58cc3b17a633e898959b1a587c0bd80
michaelkain <michael.kain@inria.fr>
2026-02-17 09:53:15 +0100

    Fix exception: InvalidDataAccessApiUsageException
    datasets  | org.springframework.dao.InvalidDataAccessApiUsageException: For queries with named parameters you need to provide names for method parameters; Use @Param for query method parameters, or when on Java 8+ use the javac flag -parameters

--- shanoir-ng-study-rights/src/main/java/org/shanoir/ng/study/rights/StudyUserRightsRepository.java
+++ shanoir-ng-study-rights/src/main/java/org/shanoir/ng/study/rights/StudyUserRightsRepository.java
@@ -51,12 +51,12 @@ public interface StudyUserRightsRepository extends CrudRepository<StudyUser, Lon
     Iterable<StudyUser> findByStudyId(Long studyId);
 
     @Query("select su.studyId from StudyUser su where su.userId = :userId and :right in elements(su.studyUserRights)")
-    List<Long> findDistinctStudyIdByUserId(Long userId, int right);
+    List<Long> findDistinctStudyIdByUserId(@Param("userId") Long userId, @Param("right") Integer right);
 
     @Query("select su from StudyUser su where su.userId = :userId and :right in elements(su.studyUserRights)")
-    Iterable<StudyUser> findByUserIdAndRight(Long userId, int right);
+    Iterable<StudyUser> findByUserIdAndRight(@Param("userId") Long userId, @Param("right") Integer right);
 
     @Query("select su from StudyUser su where su.studyId = :studyId and :right in elements(su.studyUserRights)")
-    Iterable<StudyUser> findByStudyIdAndRight(Long studyId, int right);
+    Iterable<StudyUser> findByStudyIdAndRight(@Param("studyId") Long studyId, @Param("right") Integer right);
 
 }
