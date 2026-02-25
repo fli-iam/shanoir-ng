9374343f54a56e0e493d11a83f2a4294cd0bdd40
Duckflip <guewen.hubert@inria.fr>
2026-02-25 12:11:28 +0100

    #3325 : access requests bugfixes

--- shanoir-ng-front/src/app/users/access-request/access-request-list.component.ts
+++ shanoir-ng-front/src/app/users/access-request/access-request-list.component.ts
@@ -2,12 +2,12 @@
  * Shanoir NG - Import, manage and share neuroimaging data
  * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
  * Contact us on https://project.inria.fr/shanoir/
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -16,10 +16,11 @@ import { Component } from '@angular/core';
 import { BreadcrumbsService } from '../../breadcrumbs/breadcrumbs.service';
 import { disapearUp } from '../../shared/animations/animations';
 import { UserService } from '../shared/user.service';
+import {StudyService} from "../../studies/shared/study.service";
+import {KeycloakService} from "../../shared/keycloak/keycloak.service";
 
 import { AccessRequest } from './access-request.model';
 import { AccessRequestService } from './access-request.service';
-
 
 @Component({
     selector: 'accessRequestList',
@@ -32,16 +33,28 @@ import { AccessRequestService } from './access-request.service';
 export class AccessRequestListComponent {
 
     accessRequests: AccessRequest[] = [];
+    accountCreations: AccessRequest[] = [];
 
     constructor(
             public userService: UserService,
             public accessRequestService: AccessRequestService,
-            public breadcrumbsService: BreadcrumbsService) {
+            public studyService: StudyService,
+            public breadcrumbsService: BreadcrumbsService,
+            public keycloakService: KeycloakService) {
 
-        userService.getAccessRequestsForAdmin().then(accessRequests => this.accessRequests = accessRequests);
+        userService.getAccessRequestsForAdmin().then(accessRequests => {
+            for (const accessRequest of accessRequests) {
+
+                if (accessRequest.motivation.includes("Associated email")) {
+                    this.accountCreations.push(accessRequest);
+                } else {
+                    this.accessRequests.push(accessRequest);
+                }
+            }
+        });
         setTimeout(() => {
             breadcrumbsService.currentStepAsMilestone();
-            breadcrumbsService.currentStep.label = 'Acces Requests';
+            breadcrumbsService.currentStep.label = 'Access Requests';
         });
     }
 
@@ -49,5 +62,9 @@ export class AccessRequestListComponent {
         this.accessRequestService.resolveRequest(this.accessRequests[index].id, accept);
         this.accessRequests.splice(index, 1);
         this.userService.decreaseAccessRequests();
+    }
+
+    public isAdmin(): boolean {
+        return this.keycloakService.isUserAdmin();
     }
 }
