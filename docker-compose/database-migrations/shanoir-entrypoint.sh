87227bb48254a9f0d61790d7ceddb142746b22af
Adam Fragkiadakis <adamfrag@gmail.com>
2026-03-05 13:36:09 +0100

    Adapt migrations entrypoint for standalone container
    Signed-off-by: Adam Fragkiadakis <adamfrag@gmail.com>

--- docker-compose/database-migrations/shanoir-entrypoint.sh
+++ docker-compose/database-migrations/shanoir-entrypoint.sh
@@ -7,7 +7,7 @@
 # - the 'migrations.migrations' mysql table stores the list of migrations
 #   already applied ("<DBNAME>/<SCRIPT>.sql")
 #
-# - migration are applied in alphebetical order of the filename ("<SCRIPT>.sql")
+# - migration are applied in alphabetical order of the filename ("<SCRIPT>.sql")
 #
 # - on container startup the behaviour is selected by the SHANOIR_MIGRATION
 #   environment variable:
@@ -35,18 +35,19 @@ DB_CHANGES_DIR="/opt/db-changes"
 MIGRATION_DB=migrations
 MIGRATION_USER=migrations
 MIGRATION_PASSWORD=password
+MYSQL_HOST="${MYSQL_HOST:-database}"
 
 HEADER="[Shanoir Entrypoint]"
 
-MYSQL="mysql           -u$MIGRATION_USER -p$MIGRATION_PASSWORD"
-MYSQLADMIN="mysqladmin -u$MIGRATION_USER -p$MIGRATION_PASSWORD"
+MYSQL="mysql           -h$MYSQL_HOST -u$MIGRATION_USER -p$MIGRATION_PASSWORD"
+MYSQLADMIN="mysqladmin -h$MYSQL_HOST -u$MIGRATION_USER -p$MIGRATION_PASSWORD"
 
 # wait until the mysqld server is ready
 wait_mysqld()
 {
 	echo "$HEADER wait mysqld"
 	for i in {30..0} ; do
-		if [ -f /var/lib/mysql-files/mysql-init-complete ] && $MYSQLADMIN ping ; then
+		if $MYSQLADMIN ping --silent; then
 			echo "$HEADER wait mysqld done"
 			break
 		fi
@@ -54,18 +55,9 @@ wait_mysqld()
 		sleep 1
 	done
 	if [ "$i" = 0 ] ; then
-		echo >&2 "$HEADER Timeout during MySQL init."
+		echo >&2 "$HEADER Timeout during MySQL init  at $MYSQL_HOST."
 		exit 1
 	fi
-}
-
-# send SIGTERM to mysqld and wait for its termination
-stop_mysqld()
-{
-	echo "$HEADER shutting down mysqld"
-	kill "$mysqld_pid"
-	wait
-	echo "$HEADER done"
 }
 
 # return true if the migrations table exists
@@ -129,7 +121,7 @@ apply_migrations()
 	do
 		applied_migrations[$migration]=1
 	done
-	
+
 	# process migrations in filename order (alphabetical)
 	local db
 	local status
@@ -159,82 +151,56 @@ apply_migrations()
 	return 0
 }
 
-echo "$HEADER Shanoir NG entrypoint"
+echo "$HEADER Shanoir NG migrations entrypoint"
+echo "$HEADER migration mode '$SHANOIR_MIGRATION'"
 
+case "$SHANOIR_MIGRATION" in
+init)
+  # init mode
+  # - check if the db migrations exists
+  #   - if no, initialise the migrations db
+  #   - if yes, apply de migrations
+  # - exit
 
-# If command starts with an option, then the mysql entrypoint.sh prepend mysqld
-if [ "${1:0:1}" = '-' ] ; then
-	set -- mysqld "$@"
-fi
-
-# Bypass the migration code if not running mysqld
-if [ "$1" = mysqld ] ; then
-	echo "$HEADER migration mode '$SHANOIR_MIGRATION'"
-
-	case "$SHANOIR_MIGRATION" in
-	init)
-		# init mode
-		# - run mysqld in a background process
-		# - check if the db migrations exists
-		#   - if no, initialise the migrations db
-		#   - if yes, apply de migrations
-		# - stop mysqld
-		# - exit
-
-		# TODO: clear the db dir?
-		/entrypoint.sh "$@" &
-		mysqld_pid=$!
-		wait_mysqld
-		trap stop_mysqld EXIT
-		init_migrations
-		exit $?
-		;;
-	never)
-		# TODO: (warn/fail/alter healthcheck) if there are pending migrations
-		;;
-	auto)
-		# automatic mode
-		# - run mysqld as PID 1
-		# - init db or apply migrations in a background process
-		(
-			wait_mysqld
-			if check_migrations_db ; then
-				apply_migrations
-			else
-				init_migrations
-			fi
-			if [ $? -ne 0 ] ; then
-				# fail fast
-				echo "migration failed, killing the mysql daemon..."
-				kill 1
-			fi
-		)&
-		;;
-	manual)
-		# manual mode
-		# - run mysqld in a background process
-		# - apply migrations
-		# - stop mysqld
-		# - exit
-		/entrypoint.sh "$@" &
-		mysqld_pid=$!
-		wait_mysqld
-		trap stop_mysqld EXIT
-		apply_migrations
-		exit $?
-		;;
-	
-	dev)
-		;;
-
-	'')
-		echo "$HEADER error: SHANOIR_MIGRATION is unset" >&2
-		exit 1
-		;;
-	*)
-		echo "$HEADER error: invalid SHANOIR_MIGRATION value: '$SHANOIR_MIGRATION'" >&2
-		exit
-	esac
-fi
+  wait_mysqld
+  init_migrations
+  exit $?
+  ;;
+never)
+  # TODO: (warn/fail/alter healthcheck) if there are pending migrations
+  ;;
+auto)
+  # automatic mode
+  # - init db or apply migrations in a background process
+  wait_mysqld
+  if check_migrations_db ; then
+    apply_migrations
+  else
+    init_migrations
+  fi
+  exit $?
+  ;;
+manual)
+  # manual mode
+  # - run mysqld in a background process
+  # - apply migrations
+  # - stop mysqld
+  # - exit
+  wait_mysqld
+  apply_migrations
+  exit $?
+  ;;
+dev)
+  echo "$HEADER mode '$SHANOIR_MIGRATION': nothing to do, exiting"
+  exit 0
+  ;;
+'')
+  echo "$HEADER error: SHANOIR_MIGRATION is unset" >&2
+  exit 1
+  ;;
+*)
+  echo "$HEADER error: invalid SHANOIR_MIGRATION value: '$SHANOIR_MIGRATION'" >&2
+  exit 1
+esac
 
 exec /entrypoint.sh "$@"
