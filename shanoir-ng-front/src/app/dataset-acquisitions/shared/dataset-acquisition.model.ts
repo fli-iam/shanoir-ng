6c5b3c793e6eb3d6a82422fcf4a2e41988cc60f5
LaurentPV <laurentpaulvallet@gmail.com>
2026-04-09 16:59:46 +0200

    eslint code check

--- shanoir-ng-front/src/app/dataset-acquisitions/shared/dataset-acquisition.model.ts
+++ shanoir-ng-front/src/app/dataset-acquisitions/shared/dataset-acquisition.model.ts
@@ -12,8 +12,8 @@
  * anumber with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
  */
 import { Field } from 'src/app/shared/reflect/field.decorator';
-
 import { QualityTag } from 'src/app/study-cards/shared/quality-card.model';
+
 import { AcquisitionEquipment } from '../../acquisition-equipments/shared/acquisition-equipment.model';
 import { Dataset } from '../../datasets/shared/dataset.model';
 import { Examination } from '../../examinations/shared/examination.model';
