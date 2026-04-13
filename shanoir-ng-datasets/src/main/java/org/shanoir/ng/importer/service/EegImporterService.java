ab5bb0be3f6a749d58957def1c4d8f22588dc6cf
michaelkain <michael.kain@inria.fr>
2026-04-13 15:10:16 +0200

    Fix

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/EegImporterService.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/importer/service/EegImporterService.java
@@ -155,7 +155,7 @@ public class EegImporterService {
                             }
                             String path = storageService.storeDatasetsData(
                                     importJob.getStudyId(), importJob.getSubjectId(), importJob.getExaminationId(),
-                                    BidsDataType.EEG.toString(), file.getName(),
+                                    BidsDataType.EEG.name().toString(), file.getName(),
                                     is, contentType, file.length());
                             // Create datasetExpression => Files
                             if (path != null) {
