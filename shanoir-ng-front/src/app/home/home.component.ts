0a1c6873ce849076232a8b34aac4b34725bc6c7e
Julien Louis <jul.louis@gmail.com>
2026-04-29 09:06:31 +0200

    remove unused import

--- shanoir-ng-front/src/app/home/home.component.ts
+++ shanoir-ng-front/src/app/home/home.component.ts
@@ -19,7 +19,6 @@ import { TaskService } from '../async-tasks/task.service';
 import { BreadcrumbsService } from '../breadcrumbs/breadcrumbs.service';
 import { DataUserAgreement } from '../dua/shared/dua.model';
 import { KeycloakService } from '../shared/keycloak/keycloak.service';
-import { ImagesUrlUtil } from '../shared/utils/images-url.util';
 import { StudyLight } from '../studies/shared/study.dto';
 import { StudyService } from '../studies/shared/study.service';
 import { AccessRequest } from '../users/access-request/access-request.model';
