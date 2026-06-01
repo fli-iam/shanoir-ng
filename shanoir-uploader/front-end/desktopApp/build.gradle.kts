7e4e0c2e77850a429cad9c54f42b54fa2af3861b
Estevan ROLLAND <rollandestevan.pro@gmail.com>
2026-06-01 15:27:39 +0200

    creation of sh-up front-end kotlin project

---
+++ shanoir-uploader/front-end/desktopApp/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+dependencies {
+    implementation(projects.shared)
+
+    implementation(compose.desktop.currentOs)
+    implementation(libs.kotlinx.coroutinesSwing)
+
+    implementation(libs.compose.uiToolingPreview)
+}
+
+compose.desktop {
+    application {
+        mainClass = "org.example.front_end.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.example.front_end"
+            packageVersion = "1.0.0"
+        }
+    }
+}
