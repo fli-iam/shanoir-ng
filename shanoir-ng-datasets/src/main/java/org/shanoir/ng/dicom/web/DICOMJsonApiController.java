19c4b8e88b9f51a1303a7531d5f3b0b62bfa818b
Duckflip <guewen.hubert@inria.fr>
2026-07-07 16:08:12 +0200

    #3520 : DatasetAcquisition management : datasets relation : Running examinationToStudyDTOMapper.examinationsToStudyDTOs() in a Transaction

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/dicom/web/DICOMJsonApiController.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/dicom/web/DICOMJsonApiController.java
@@ -21,6 +21,7 @@ import org.shanoir.ng.dicom.web.dto.StudyDTO;
 import org.shanoir.ng.dicom.web.dto.mapper.ExaminationToStudyDTOMapper;
 import org.shanoir.ng.examination.model.Examination;
 import org.shanoir.ng.examination.service.ExaminationService;
+import org.shanoir.ng.shared.service.TransactionRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,7 +43,9 @@ public class DICOMJsonApiController implements DICOMJsonApi {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
-        List<StudyDTO> studies = examinationToStudyDTOMapper.examinationsToStudyDTOs(examinations);
+        TransactionRunner tr = new TransactionRunner();
+
+        List<StudyDTO> studies = tr.callInTransaction(em -> examinationToStudyDTOMapper.examinationsToStudyDTOs(examinations));
         StudiesDTO studiesDTO = new StudiesDTO();
         studiesDTO.setStudies(studies);
         return new ResponseEntity<StudiesDTO>(studiesDTO, HttpStatus.OK);
