7a56d53e7f6deeecc151357f4e6af9b2996bbe8b
Adam Fragkiadakis <adamfrag@gmail.com>
2026-07-08 16:57:57 +0200

    Drop `subject_study_tag` and `subject_study` tables; migrate `study_id` to `subject` where missing.
    Signed-off-by: Adam Fragkiadakis <adamfrag@gmail.com>

--- docker-compose/database-migrations/db-changes/studies/0032_drop_subject_study_tag.sql
+++ docker-compose/database-migrations/db-changes/studies/0032_drop_subject_study_tag.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS subject_study_tag;
+UPDATE subject s INNER JOIN subject_study ss ON ss.subject_id = s.id SET s.study_id = ss.study_id WHERE s.study_id IS NULL;
+DROP TABLE IF EXISTS subject_study;
