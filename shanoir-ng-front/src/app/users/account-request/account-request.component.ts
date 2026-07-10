862907d2c25e1d18783df20a9f09ad39e0bc2068
Duckflip <guewen.hubert@inria.fr>
2026-07-10 16:03:29 +0200

    #3651: set up mail supplier black list + related env var
    # Conflicts:
    #	.env
    #	docker-compose-dev.yml
    #	docker-compose.yml
    #	shanoir-ng-front/src/assets/env.js
    #	shanoir-ng-front/src/environments/environment.prod.ts
    #	shanoir-ng-front/src/environments/environment.ts

--- shanoir-ng-front/src/app/users/account-request/account-request.component.ts
+++ shanoir-ng-front/src/app/users/account-request/account-request.component.ts
@@ -19,6 +19,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { ConsoleService } from '@app/shared/console/console.service';
 
+import mailSupplierBlacklists from '../../../assets/mail_supplier_blacklist.json';
 import * as AppUtils from '../../utils/app.utils';
 import { AccountRequestInfo } from '../account-request-info/account-request-info.model';
 import { User } from '../shared/user.model';
@@ -37,6 +38,7 @@ import { ConsoleComponent } from '../../shared/console/console.component';
 
 export class AccountRequestComponent implements OnInit {
 
+
     public form: UntypedFormGroup;
     public dpoMail: string = AppUtils.SHANOIR_DPO_EMAIL;
     public requestSent: boolean = false;
@@ -48,6 +50,9 @@ export class AccountRequestComponent implements OnInit {
     function: string; // optional : operator/researcher
     language: 'english' | 'french' = 'english';
     loading: boolean = false;
+    public mailSupplierBlacklistConfig: string = AppUtils.MAIL_SUPPLIER_BLACK_LIST;
+    public mailSupplierBlacklist: string[] = [];
+
 
     constructor(
             private fb: UntypedFormBuilder,
@@ -62,6 +67,7 @@ export class AccountRequestComponent implements OnInit {
             }
 
     ngOnInit(): void {
+        this.loadMailSupplierBlackList();
         this.buildForm();
     }
 
@@ -70,7 +76,7 @@ export class AccountRequestComponent implements OnInit {
         this.form = this.fb.group({
             'firstName': ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.nonSpecialCharsValidator()]],
             'lastName': ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.nonSpecialCharsValidator()]],
-            'email': ['', [Validators.required, Validators.pattern(emailRegex)]],
+            'email': ['', [Validators.required, Validators.pattern(emailRegex), this.nonBlacklistedValidator()]],
             'accountRequestInfo': [new AccountRequestInfo(), [this.validateARInfo]]
         });
     }
@@ -146,5 +152,23 @@ export class AccountRequestComponent implements OnInit {
                 return /^[\p{L}\p{M}\s'-]+$/u.test(control.value) ? null : { invalidName: true };            }
             return null;
         };
+    }
+
+    private nonBlacklistedValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            if (control.value != undefined && this.mailSupplierBlacklist.length > 0) {
+                const domain: string = control.value.split('@')[1]?.toLowerCase();
+                if (domain && this.mailSupplierBlacklist.some(supplier => domain === supplier || domain.endsWith('.' + supplier))) {
+                    return { blacklist: true };
+                }
+            }
+            return null;
+        };
+    }
+
+    private loadMailSupplierBlackList() {
+        if (this.mailSupplierBlacklistConfig) {
+            this.mailSupplierBlacklist = (mailSupplierBlacklists as Record<string, string[]>)[this.mailSupplierBlacklistConfig] || [];
+        }
     }
 }
