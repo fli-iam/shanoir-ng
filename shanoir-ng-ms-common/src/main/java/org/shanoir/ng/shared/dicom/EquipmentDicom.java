e6bc07cb7afd7ef82af7f37f25acac2b103ee5f2
michaelkain <michael.kain@inria.fr>
2026-07-16 09:35:36 +0200

    Update EquipmentDicom.java

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/dicom/EquipmentDicom.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/dicom/EquipmentDicom.java
@@ -26,7 +26,7 @@ public class EquipmentDicom {
 
     private static final String UNKNOWN = "unknown";
 
-    private static final String _0_0 = "0.0";
+    private static final String MAGNETIC_FIELD_STRENGTH_DEFAULT = "0.0";
 
     @JsonProperty("manufacturer")
     private String manufacturer;
@@ -55,7 +55,8 @@ public class EquipmentDicom {
         this.manufacturerModelName = Utils.getOrSetToDefault(attributes, Tag.ManufacturerModelName, UNKNOWN);
         this.deviceSerialNumber = Utils.getOrSetToDefault(attributes, Tag.DeviceSerialNumber, UNKNOWN);
         this.stationName = Utils.getOrSetToDefault(attributes, Tag.StationName, UNKNOWN);
-        this.magneticFieldStrength = Utils.getOrSetToDefault(attributes, Tag.MagneticFieldStrength, _0_0);
+        this.magneticFieldStrength = Utils.getOrSetToDefault(attributes, Tag.MagneticFieldStrength,
+                MAGNETIC_FIELD_STRENGTH_DEFAULT);
     }
 
     public String getManufacturer() {
