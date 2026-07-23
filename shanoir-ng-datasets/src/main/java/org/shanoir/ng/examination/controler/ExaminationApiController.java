fff0fa8e450b8db7169deb8132e6bf2a57cfa782
michaelkain <michael.kain@inria.fr>
2026-07-23 15:44:47 +0200

    Update ExaminationApiController.java

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/controler/ExaminationApiController.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/examination/controler/ExaminationApiController.java
@@ -74,7 +74,7 @@ public class ExaminationApiController implements ExaminationApi {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExaminationApiController.class);
 
-    private static UIDGeneration GENERATOR = new UIDGeneration();
+    private static final UIDGeneration UID_GENERATOR = new UIDGeneration();
 
     @Autowired
     private ExaminationMapper examinationMapper;
@@ -376,7 +376,7 @@ public class ExaminationApiController implements ExaminationApi {
      * @param examination
      */
     private void generateStudyInstanceUID(Examination examination) {
-        String newUID = GENERATOR.getNewUID();
+        String newUID = UID_GENERATOR.getNewUID();
         examination.setStudyInstanceUID(newUID);
     }
 
