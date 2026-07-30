25b6b7b7539f75e055a4ef70d29bf3f35371d296
Julien Louis <jul.louis@gmail.com>
2026-07-30 12:53:51 +0200

    add missing indexes

---
+++ docker-compose/database-migrations/db-changes/datasets/0078_missing_fk_indexes.sql
@@ -0,0 +1,17 @@
+-- Shanoir NG - Import, manage and share neuroimaging data
+-- Copyright (C) 2009-2019 Inria - https://www.inria.fr/
+-- Contact us on https://project.inria.fr/shanoir/
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.htm
+
+CREATE INDEX FKa6tmrrsalv4mmwcoc0c9mlco9 ON examination (study_id);
+
+CREATE INDEX FKbyupgcuub9vxh5s5uu258uewy ON examination (subject_id);
+
+CREATE INDEX FKle0p9n78s9xv90qpp7gll7yl6 ON examination (source_id);
