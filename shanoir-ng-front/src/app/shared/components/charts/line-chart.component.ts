02de637f3280d5ee99c30630ad513a41c18b18d1
LaurentPV <laurentpaulvallet@gmail.com>
2026-08-12 10:11:37 +0200

    ading line chart component to create main inclusion evolution chart

---
+++ shanoir-ng-front/src/app/shared/components/charts/line-chart.component.ts
@@ -0,0 +1,67 @@
+/**
+ * Shanoir NG - Import, manage and share neuroimaging data
+ * Copyright (C) 2009-2019 Inria - https://www.inria.fr/
+ * Contact us on https://project.inria.fr/shanoir/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+import { Component, Input, OnChanges } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+export interface LineChartDataset {
+    label: string;
+    data: number[];
+    color?: string;
+}
+
+@Component({
+    selector: 'line-chart',
+    templateUrl: 'line-chart.component.html',
+    styleUrls: ['line-chart.component.css'],
+    imports: [BaseChartDirective]
+})
+export class LineChartComponent implements OnChanges {
+
+    @Input() labels: string[] = [];
+    @Input() datasets: LineChartDataset[] = [];
+    @Input() yAxisLabel: string;
+
+    chartData: ChartConfiguration<'line'>['data'];
+    chartOptions: ChartOptions<'line'>;
+
+    ngOnChanges(): void {
+        this.chartData = {
+            labels: this.labels,
+            datasets: this.datasets.map(ds => ({
+                label: ds.label,
+                data: ds.data,
+                borderColor: ds.color,
+                backgroundColor: ds.color,
+                fill: false,
+                tension: 0.2,
+                pointRadius: 2,
+            })),
+        };
+        this.chartOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: { display: !!this.yAxisLabel, text: this.yAxisLabel },
+                },
+            },
+            plugins: {
+                legend: { display: true },
+            },
+        };
+    }
+}
