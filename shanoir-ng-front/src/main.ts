aaf79d28551a9a8cb7d73d254e60ccc84e576293
LaurentPV <laurentpaulvallet@gmail.com>
2026-08-13 10:24:08 +0200

    adding ng2-charts package

--- shanoir-ng-front/src/main.ts
+++ shanoir-ng-front/src/main.ts
@@ -9,6 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 import { AcquisitionEquipmentPipe } from './app/acquisition-equipments/shared/acquisition-equipment.pipe';
 import { AcquisitionEquipmentService } from './app/acquisition-equipments/shared/acquisition-equipment.service';
@@ -202,6 +203,7 @@ const options = {
         ExecutionTemplateFilterService,
         { provide: HTTP_INTERCEPTORS, useClass: ShanoirHttpInterceptor, multi: true },
         provideHttpClient(withInterceptorsFromDi()),
+        provideCharts(withDefaultRegisterables()),
     ]
 }
 
