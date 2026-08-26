45d5e0f68d531810d6a947a7c8e76b3b342b2bd5
michaelkain <michael.kain@inria.fr>
2026-08-26 17:08:40 +0200

    Add missing class in common-jackson3

--- shanoir-ng-ms-common-jackson3/src/main/java/org/shanoir/ng/shared/event/ShanoirEvent.java
+++ shanoir-ng-ms-common-jackson3/src/main/java/org/shanoir/ng/shared/event/ShanoirEvent.java
@@ -14,10 +14,12 @@
 
 package org.shanoir.ng.shared.event;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Transient;
 import java.util.Map;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Transient;
 
 public class ShanoirEvent {
 
