9c084eac619593ab7b488be81c61671cf67413c5
michaelkain <michael.kain@inria.fr>
2026-08-28 11:00:18 +0200

    Update ShanoirEventService.java

--- shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/event/ShanoirEventService.java
+++ shanoir-ng-ms-common/src/main/java/org/shanoir/ng/shared/event/ShanoirEventService.java
@@ -21,8 +21,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
-
 /**
  * Service to send every event created.
  *
