8112aa89b56af66b464ea6c08c5f4d3a4763b402
michaelkain <michael.kain@inria.fr>
2026-09-23 13:48:15 +0200

    Update JacksonUsersConfiguration.java

--- shanoir-ng-users/src/main/java/org/shanoir/ng/configuration/JacksonUsersConfiguration.java
+++ shanoir-ng-users/src/main/java/org/shanoir/ng/configuration/JacksonUsersConfiguration.java
@@ -18,6 +18,7 @@ import org.shanoir.ng.study.rights.StudyUser;
 import org.shanoir.ng.study.rights.StudyUserInterface;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -34,9 +35,10 @@ import tools.jackson.databind.module.SimpleModule;
 import tools.jackson.databind.ser.ValueSerializerModifier;
 
 @Configuration(proxyBeanMethods = false)
-public class JacksonConfiguration {
+public class JacksonUsersConfiguration {
 
     @Bean
+    @Primary
     JsonMapper jsonMapper() {
         SimpleModule studyUserModule = new SimpleModule();
         studyUserModule.addAbstractTypeMapping(StudyUserInterface.class, StudyUser.class);
