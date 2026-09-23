f63980904d418f043cf3e78346c700405896fce0
michaelkain <michael.kain@inria.fr>
2026-09-23 09:38:15 +0200

    Update RabbitMQImportService.java

--- shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/amqp/RabbitMQImportService.java
+++ shanoir-ng-import/src/main/java/org/shanoir/ng/configuration/amqp/RabbitMQImportService.java
@@ -14,8 +14,11 @@
 
 package org.shanoir.ng.configuration.amqp;
 
+import java.util.List;
+
 import org.shanoir.ng.shared.configuration.RabbitMQConfiguration;
 import org.shanoir.ng.study.rights.ampq.RabbitMqStudyUserService;
+import org.shanoir.ng.study.rights.command.StudyUserCommand;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -38,8 +41,8 @@ public class RabbitMQImportService {
             exchange = @Exchange(value = RabbitMQConfiguration.STUDY_USER_EXCHANGE, ignoreDeclarationExceptions = "true",
                 autoDelete = "false", durable = "true", type = ExchangeTypes.FANOUT)), containerFactory = "multipleConsumersFactory"
     )
-    public void receiveMessage(String commandArrStr) {
-        listener.receiveStudyUsers(commandArrStr);
+    public void receiveMessage(List<StudyUserCommand> commands) {
+        listener.receiveStudyUsers(commands);
     }
 
 }
